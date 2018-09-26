e99c1398b83d337669847ef106f8c81e47f8f280
Calem R Hoffman <calemhoffman@visitor-37611399.dyndns.cern.ch>
2018-09-26 21:52:39 +0200

    new calibrations and new cutcreator

--- analysis/working/CutCreator.C
+++ analysis/working/CutCreator.C
@@ -1,5 +1,8 @@
 #include <TH2F.h>
 #include <TFile.h>
+#include <TChain.h>
+#include <TROOT.h>
+#include <TStyle.h>
 #include <TCanvas.h>
 #include <TCutG.h>
 #include <TString.h>
@@ -7,71 +10,65 @@
 
 void CutCreator(){
 	
-	printf("================ Graphic Cut Creator for ISS ============== \n");
-	/*
-	printf("1 = Recoil cuts \n");
-	printf("2 = E vs. Z cut \n");
-	printf("============== Choice? : ");
-	*/
-	//scanf("%d", &option);
-	
+	printf("================ Graphic Cut Creator for RDT ============== \n");
+   
+   TChain * chain = new TChain("gen_tree");
+   chain->Add("../../../root_data/gen_run48.root");
+   //chain->Add("data/gen_run49.root");
+   //chain->Add("data/gen_run50.root");
+   
+   chain->GetListOfFiles()->Print();
+   
 	TString varX, varY, tag;
 		
 	gStyle->SetOptStat(00000);
 	
-	TCanvas * cCutCreator = new TCanvas("cCutCreator", "Cut Creator", 100, 100, 800, 800);
+	TCanvas * cCutCreator = new TCanvas("cCutCreator", "RDT Cut Creator", 100, 100, 800, 800);
 	if( !cCutCreator->GetShowToolBar() ) cCutCreator->ToggleToolBar();
 	
-	TString expression[10];
-
-	for (Int_t i=0;i<4;i++) {
-	  varX.Form("rdt[%d]",i+4); varY.Form("rdt[%d]",i);
-	  
-	  expression[i].Form("%s:%s>>h(2096, 0, 8192, 2096, 0, 8192)", 
-			     varY.Data(),
-			     varX.Data());
-	}
-
-
-	TFile * cutFile = new TFile("cutsFile.root", "recreate");
+   
+   TFile * cutFile = new TFile("rdtCuts.root", "recreate");
 	cCutCreator->Update();
 	
 	TCutG * cut = NULL;
 	TObjArray * cutList = new TObjArray();
 	
-	int count = 1;
-	do{
-	  printf("======== make a graphic cut on the plot (double click on plot to stop) : " );
+   
+	TString expression[10];
 
-	  gen_tree->Draw(expression[count-1], "", "col");
-	  cCutCreator->Modified(); cCutCreator->Update();
-	  
-	  gPad->WaitPrimitive();
-	  
-	  cut = (TCutG*) gROOT->FindObject("CUTG");
-	  
-	  if( cut == NULL) {
-	    printf(" break \n");
-	    count --;
-	    break;
-	  }
-	  
-	  TString name; name.Form("cut%d", count);
-	  cut->SetName(name);
-	  cut->SetVarX(varX.Data());
-	  cut->SetVarY(varY.Data());
-	  cut->SetTitle(tag);
-	  cut->SetLineColor(count);
-	  cutList->Add(cut);
-	  //cut->Write();
-	  
-	  printf(" cut-%d \n", count);			
-	  count++;
-	  
-	}while( cut != NULL );
+	for (Int_t i = 0; i < 4; i++) {
+
+      printf("======== make a graphic cut on the plot, %d-th cut: ", i );
+
+      varX.Form("rdt[%d]",i+4); varY.Form("rdt[%d]",i);
+
+      expression[i].Form("%s:%s>>h(2096, 0, 8192, 2096, 0, 8192)", 
+            varY.Data(),
+            varX.Data());
+
+
+      chain->Draw(expression[i], "", "col");
+      cCutCreator->Modified(); cCutCreator->Update();
+
+      gPad->WaitPrimitive();
+
+      cut = (TCutG*) gROOT->FindObject("CUTG");
+
+      TString name; name.Form("cut%d", i);
+      cut->SetName(name);
+      cut->SetVarX(varX.Data());
+      cut->SetVarY(varY.Data());
+      cut->SetTitle(tag);
+      cut->SetLineColor(i);
+      cutList->Add(cut);
+
+      printf(" cut-%d \n", i);
+              
+     
+	}
 	
 	cutList->Write("cutList", TObject::kSingleKey);
 	
-	printf("====> saved %d cuts into cutsFile.root\n", count);
+	printf("====> saved %d cuts into rdtCuts.root\n", 4);
 	
 }
