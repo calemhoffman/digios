7e21633c520e2accfd035cae84f2aeaa5e1fe77c
Ryan_Mac <ttang@anl.gov>
2018-11-29 17:47:41 -0600

    change sort_codes to Armory

--- analysis/working/process_run.sh
+++ analysis/working/process_run.sh
@@ -3,23 +3,24 @@
 if [ $# -eq 0 ] ; then
    echo "$./process_run_simple.sh #RUNNUM "
    echo "$./process_run_simple.sh #RUNNUM #Option_for_single_run"
+                                          #If option = 1, run all processed runs, else, only process this run
   exit 1
 fi;
 
 exp=iss631
-dir=/Users/heliosdigios/experiments/${exp}
+AnalysisDir=/Users/ryantang/Desktop/HELIOS_repos/analysis
 
 #remote data path
 dataloc=/media/DIGIOSDATA3/data/${exp}
 daqDir=/home/helios/experiments/iss631
 
 #===== directory and chat files
-GEBDIR=$dir/GEBSort
-MERGDIR=$dir/merged_data
-ROOTDIR=$dir/root_data
-DATADIR=$dir/data
-MERGECHAT=$dir/working/GEBMerge.chat
-SORTCHAT=$dir/working/GEBSort.chat
+GEBDIR=$AnalysisDir/GEBSort
+MERGDIR=$AnalysisDir/merged_data
+ROOTDIR=$AnalysisDir/root_data
+DATADIR=$AnalysisDir/data
+MERGECHAT=$AnalysisDir/working/GEBMerge.chat
+SORTCHAT=$AnalysisDir/working/GEBSort.chat
 
 
 RUN=$1
@@ -32,12 +33,12 @@ if [ $# -eq 2 ] ; then
   echo "=================== single run ."
 fi
 
+#============ Get the raw data
 echo "RUN $RUN: Get the raw data `date`"
-
 rsync -avuht --progress "helios@anldaqrouter:${dataloc}/${exp}_run_$RUN.gtd*" ${DATADIR}/.
-rsync -avuht --progress "helios@anldaqrouter:${daqDir}/RunTimeStamp.txt" ${dir}/working/.
+rsync -avuht --progress "helios@anldaqrouter:${daqDir}/RunTimeStamp.txt" ${AnalysisDir}/working/.
 echo "============================================="
-cat ${dir}/working/RunTimeStamp.txt
+cat ${AnalysisDir}/working/RunTimeStamp.txt
 echo "============================================="
 
 du -hsc $DATADIR/${exp}_run_$RUN*
@@ -49,6 +50,7 @@ if [ ! -f $DATADIR/${exp}_run_$RUN.gtd03_000_0110 ]; then
     exit 
 fi;
 
+#=========== Merge and Sort
 echo "RUN $RUN: GEBMerge started at `date`"
 $GEBDIR/GEBMerge $MERGECHAT  $MERGDIR/GEBMerged_run$RUN.gtd `ls $DATADIR/${exp}_run_$RUN.gtd*` > $MERGDIR/GEBMerge_run$RUN.log
 echo "RUN $RUN: GEBMerge DONE at `date`"
@@ -62,6 +64,7 @@ echo "saved root file -->  "  $ROOTDIR/run$RUN.root
 echo "============================================="
 echo "============================================="
 
+#========== convert to normal number, without zero in front
 if [ "${RUN:0:1}" == "0" ] ; then
       runID=${RUN:1:2}
 else
@@ -69,9 +72,9 @@ else
 fi;
 
 root -q -b "process_run.C($runID,0)"
-
 root -l ../sort_codes/runsCheck.C
 
+#=========== If option = 1, run all processed runs, else, only process this run
 if [ $# -eq 1 ] ; then
   root -l Chain.C
 else
