7c0f90c12f82f6efb363420d887f38f976319b78
HELIOS_digios <helios_digios@anl.gov>
2019-04-05 19:42:43 -0500

    DAQ : bug fix on databaseVME_Threshold.sh

--- daq/edm/scripts/databaseVME_Threshold.sh
+++ daq/edm/scripts/databaseVME_Threshold.sh
@@ -1,17 +1,18 @@
 #!/bin/bash
 
 if [ $# -ne 1 ]; then
-    echo "Please enter waiting time in sec"
+    echo "Please enter waiting time in sec, 0 for 1-shot."
     exit 1
 fi
 
 time=$1
 
+
 VME="1 2 3 4"
 DIG="1 2 3"
 CH="0 1 2 3 4 5 6 7 8 9"
 
-loop=1
+loopflag=1
 
 while [ ${loopflag} -eq 1 ]; do
     for a in $VME ; do
@@ -26,6 +27,10 @@ while [ ${loopflag} -eq 1 ]; do
 	done
     done
 
-    sleep time
+    if [ ${time} -eq 0 ]; then
+	exit 1
+    else
+	sleep time
+    fi
 
 done
