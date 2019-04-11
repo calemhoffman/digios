1ad8ee6208c7cf191580b1f333de0efda80bb136
HELIOS_Mac <helios_mac@anl.gov>
2019-04-11 12:04:57 -0500

    Analysis Code update

--- analysis/working/Monitors.C
+++ analysis/working/Monitors.C
@@ -21,8 +21,8 @@ const int numRow = 6;
 ULong64_t maxNumberEvent = 100000000;
 
 //---histogram seeting
-int rawEnergyRange[2] = {-500, 5000}; // share with e, ring, xf, xn
-int energyRange[2]= {500, 5000};
+int rawEnergyRange[2] = {-500, 8000}; // share with e, ring, xf, xn
+int energyRange[2]= {500, 8000};
 
 //TODO switch for histogram
 
@@ -548,6 +548,9 @@ Bool_t Monitors::Process(Long64_t entry)
     for (Int_t i = 0; i < numDet; i++) {
 
       //======================= fill raw data
+      
+      //if( ring[i] !=0 ) return kTRUE;
+      
       he[i]->Fill(e[i]);
       hring[i]->Fill(ring[i]);
       hxf[i]->Fill(xf[i]);
@@ -602,14 +605,16 @@ Bool_t Monitors::Process(Long64_t entry)
       //if( !TMath::IsNaN(z[i]) ) multiHit ++;
       
       //=================== Array fill next
-      if( -1.1 < x[i] && x[i] < 1.1 && e[i]>100 && (xn[i]>0||xf[i]>0)) {
-        heVx[i]->Fill(x[i],e[i]);
-        hringVx[i]->Fill(x[i],ring[i]);
+
+      heVx[i]->Fill(x[i],e[i]);
+      hringVx[i]->Fill(x[i],ring[i]);
+     
+      //if( -1.1 < xcal[i] && xcal[i] < 1.1 && eCal[i]>100 && (xn[i]>0||xf[i]>0)) {
         heCalVxCal[i]->Fill(xcal[i]*5.0,eCal[i]);
         heCalVz->Fill(z[i]*10.,eCal[i]);
 
-        for (Int_t ii=0;ii<4;ii++) hrdtg[ii]->Fill(rdt[ii+4],rdt[ii]);
-      }
+        //for (Int_t ii=0;ii<4;ii++) hrdtg[ii]->Fill(rdt[ii+4],rdt[ii]);
+      //}
       
     }//end of array loop
     
--- analysis/working/process_run.C
+++ analysis/working/process_run.C
@@ -1,5 +1,10 @@
-void process_run(Int_t RUNNUM=5, bool isTrace=false, Int_t SORTNUM=0)
-{
+#include <TROOT.h>
+#include <TChain.h>
+#include <TTree.h>
+#include <TFile.h>
+#include <TProof.h>
+
+void process_run(Int_t RUNNUM=5, int isTrace=0, Int_t SORTNUM=0){
   
   if (SORTNUM==0) {
     TString name;
@@ -12,22 +17,39 @@ void process_run(Int_t RUNNUM=5, bool isTrace=false, Int_t SORTNUM=0)
 
     
     TString processCmd;
-    if( isTrace ) {
+    if( isTrace == 0 ) {
         processCmd.Form("../Armory/GeneralSortTrace.C+");
-    }else{
+        t1->Process(processCmd);
+        f.Close();
+    }else if(isTrace == 1){
         processCmd.Form("../Armory/GeneralSort.C+");
-    }
-    t1->Process(processCmd);
-    f.Close();
-    
-    //use cp then rm, because the ownership in ../root_data may be not the same
-    //processCmd.Form(".!cp -v gen_run%03d.root ../root_data/gen_run%03d.root", RUNNUM, RUNNUM);
-    //gROOT->ProcessLine(processCmd);
-    //processCmd.Form(".!rm -fv gen_run%03d.root", RUNNUM);
-    //gROOT->ProcessLine(processCmd);
+        t1->Process(processCmd);
+        f.Close();
+    }else if(isTrace == 2){
+        TChain * chain = new TChain("tree");
+        chain->Add(name); 
+   
+        TProof * p = TProof::Open("", "workers=4");
+   
+        chain->SetProof();
 
+        chain->Process("../Armory/GeneralSortTraceProof.C+", name);
+        
+        
+        delete chain;
+        delete p;
+        
+    }else{
+        return;
+    }
+    
     printf("======================================done RUN-%03d \n", RUNNUM);
-    name.Form("../root_data/gen_run%03d.root", RUNNUM);
+
+    if( isTrace < 2 ){
+        name.Form("../root_data/gen_run%03d.root", RUNNUM);
+    }else if ( isTrace == 2 ) {
+        name.Form("sortedTrace.root"); //TODO rename
+    }
     printf("%s \n", name.Data());
     printf("============================================= \n");
     gROOT->ProcessLine(".q");
