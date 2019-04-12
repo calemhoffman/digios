4165e5b6ad21e34ba071f417c3deec651a9192b5
HELIOS_digios <helios_digios@anl.gov>
2019-04-12 11:39:32 -0500

    update SetUpNewExp.sh from origin/master

--- SetUpNewExp.sh
+++ SetUpNewExp.sh
@@ -72,7 +72,9 @@ isBranchExist=`git branch -a | grep ${expName} | wc -l`
 if [ ${expName} == "ARR01" ]; then
    echo "this is master experiment name. no branch create."
    git checkout master
+   isBranchExist=1
 else
+
    if [ ${isBranchExist} -eq 0 ]; then
        git checkout -b ${expName}
    else
@@ -122,6 +124,7 @@ echo "=================== done."
 
 #===== clean up working if it is new
 if [ ${isBranchExist} -eq 0 ]; then
+
     echo "======== Clean up working directory "
     rm -fv correction_*.dat
     rm -fv reaction.dat
@@ -132,4 +135,5 @@ if [ ${isBranchExist} -eq 0 ]; then
     rm -fv *.d
     rm -fv *.so
     rm -fv *.pcm
+
 fi 
