287b07f87316fdd6ba10a6c63f13631cb6bee950
Ryan_LCRC <ttang@anl.gov>
2019-04-22 19:45:35 -0500

    update Armory and Cleopatra

--- analysis/Cleopatra/FindThetaCM.C
+++ analysis/Cleopatra/FindThetaCM.C
@@ -22,38 +22,37 @@ int main(int argc, char *argv[]){
   printf("===     Find ThetaCM convrage for each detector at Ex        ====\n");
   printf("=================================================================\n");
 
-  if(argc < 2 || argc > 5) { 
+  if(argc < 2 || argc > 6) { 
     printf("Usage: ./FindThetaCM Ex\n");
-    printf("Usage: ./FindThetaCM Ex X-Ratio\n");
-    printf("Usage: ./FindThetaCM Ex X-Ratio reactionTxt detGeoTxt\n");
+    printf("Usage: ./FindThetaCM Ex nDiv\n");
+    printf("Usage: ./FindThetaCM Ex nDiv X-Ratio\n");
+    printf("Usage: ./FindThetaCM Ex nDiv X-Ratio reactionTxt detGeoTxt\n");
     exit(0); 
   }  
   
   double Ex = 0;
   double xRatio = 0.95;
+  int nDiv = 0;
   string reactionTxt = "reactionConfig.txt";
   string detGeoTxt = "detectorGeo.txt";
   
-  if ( argc == 2 ){
+  if ( argc >= 2 ){
     Ex = atof(argv[1]);
   }
-  if ( argc == 3 ){
-    Ex = atof(argv[1]);
-    xRatio = atof(argv[2]);
+  if ( argc >= 3 ){
+    nDiv = atoi(argv[2]);
   }
-  if ( argc == 4 ){
-    Ex = atof(argv[1]);
-    xRatio = atof(argv[2]);
-    reactionTxt = argv[3];
+  if ( argc >= 4 ){
+    xRatio = atof(argv[3]);
   }
-  if ( argc == 5 ){
-    Ex = atof(argv[1]);
-    xRatio = atof(argv[2]);
-    reactionTxt = argv[3];
-    detGeoTxt = argv[4];
+  if ( argc >= 5 ){
+    reactionTxt = argv[4];
+  }
+  if ( argc >= 6 ){
+    detGeoTxt = argv[5];
   }
   
-  FindThetaCM(Ex, xRatio, reactionTxt, detGeoTxt);
+  FindThetaCM(Ex, nDiv, xRatio, reactionTxt, detGeoTxt);
   
   return 0;
 }
