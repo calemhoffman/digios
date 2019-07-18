95691006858cf521ca495660d7172b39a416b031
HELIOS_digios <helios_digios@anl.gov>
2019-07-18 02:18:05 -0500

    daq scripts update

--- daq/edm/scripts/stop_run.sh
+++ daq/edm/scripts/stop_run.sh
@@ -18,14 +18,27 @@ currentDate=$(date)
 
 echo "         stop at ${currentDate}"
 echo "         stop at ${currentDate}" >> ${daqDataPath}/${expName}/data/RunTimeStamp.dat
+
+#download the elog entry
+elogContent=~/digios/analysis/working/elog_content.txt
+elog -h www.phy.anl.gov -d elog -p 443 -l ${expName^^} -s -u GeneralHelios helios -w ${elogID} > ${elogContent}
+cutLineNum=$(grep -n "==============" elog_content.txt | cut -d: -f1)
+cutLineNum=$((cutLineNum+1))
+tail -n +${cutLineNum} ${elogContent} > ${elogContent} 
+
 echo "         stop at ${currentDate}" >> ~/digios/analysis/working/elog.txt
 
+# this will create seperated elog entry 
 ~/digios/daq/edm/scripts/elog.sh stop
+# in order to replace the elog entry, comment out above line, the GrafanaElog.sh in heliosdb (you have to edit GrafanaElog.sh)  will do the job
 
 caput Online_CS_StartStop Stop
 caput Online_CS_SaveData "No Save"
 
 curl -s -XPOST "http://heliosDB:8086/write?db=testing" --data-binary "SavingData,expName=${expName} value=0" --max-time 1 --connect-timeout 1
+
+echo "==== wait for 5 sec"
+sleep 5
 
 ssh heliosdatabase@heliosdb '/home/heliosdatabase/GrafanaElog.sh'
 
@@ -46,4 +59,8 @@ do
    done <temp
 done        
 rm -rf temp
+
+echo "=== wait 10 seconds before next run ==="
+sleep 10
+
 echo -e "------------ The Run\033[0;31m${RUN}\033[0m has now been STOPPED  ----------------"
