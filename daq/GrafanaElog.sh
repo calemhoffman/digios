0438533a1c2d6edf7f7b9140101364946acc9337
HELIOS_DB <helios_DB@anl.gov>
2019-09-14 13:22:09 -0500

    changed of heliosDB:daq/GrafananElog.sh

--- daq/GrafanaElog.sh
+++ daq/GrafanaElog.sh
@@ -13,14 +13,14 @@ echo "ScreeShot size : ${width} x ${higth}"
 
 convert ${screenShot} -crop 1980x1080+0+0 ${screenShot}
 
-echo "============= downloading expName.sh"
-rsync -avuht --progress "helios@digios1:~/digios/expName.sh" ~/digios/expName.sh
-rsync -avuht --progress "helios@digios1:~/digios/analysis/working/elog_context.txt" ~/elog.txt
-source ~/digios/expName.sh
-echo "============= push to elog for Grafana"
+#echo "============= downloading expName.sh"
+#rsync -avuht --progress "helios@digios1:~/digios/expName.sh" ~/digios/expName.sh
+#rsync -avuht --progress "helios@digios1:~/digios/analysis/working/elog_context.txt" ~/elog.txt
+#source ~/digios/expName.sh
+#echo "============= push to elog for Grafana"
 
 #elog -h www.phy.anl.gov -d elog -p 443 -l ${expName^^} -s -u GeneralHelios helios -a Category=Run -a RunNo=${LastRunNum} -a Subject="Grafana ScreenShot for ${LastRunNum}" -f ${screenShot} -n 1 "Grafana auto screenshot"
 
 #the folllowing is use for replacing the elog. more, see digios1:~/digios/daq/edm/scripts/stop_run.sh
-elog -h www.phy.anl.gov -d elog -p 443 -l ${expName^^} -s -u GeneralHelios helios -e ${elogID} -f ${screenShot} -n 1 ~/elog.txt
+#elog -h www.phy.anl.gov -d elog -p 443 -l ${expName^^} -s -u GeneralHelios helios -e ${elogID} -f ${screenShot} -n 1 ~/elog.txt
 
