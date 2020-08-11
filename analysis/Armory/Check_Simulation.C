e51ee04a572ec5dff2071abbcc0788f3787f44bc
Ryan <ryantang@cns.s.u-tokyo.ac.jp>
2020-08-11 17:30:57 -0500

    update simulation code from master

--- analysis/Armory/Check_Simulation.C
+++ analysis/Armory/Check_Simulation.C
@@ -19,22 +19,25 @@
 
 enum plotID { pEZ,               /// 0
               pRecoilXY,         /// 1
-              pRecoilRZ,         /// 2
-              pRecoilRTR,        /// 3
-              pTDiffZ,           /// 4
-              pThetaCM,          /// 5
-              pThetaCM_Z,        /// 6
-              pExCal,            /// 7
-              pRecoilRThetaCM,   /// 8
-              pInfo,             /// 9
-              pHitID,            /// 10
-              pEmpty };          /// 11
+              pRecoilXY1,        /// 2
+              pRecoilXY2,        /// 3
+              pRecoilRZ,         /// 4
+              pRecoilRTR,        /// 5
+              pTDiffZ,           /// 6
+              pThetaCM,          /// 7
+              pThetaCM_Z,        /// 8
+              pExCal,            /// 9
+              pRecoilRThetaCM,   /// 10
+              pArrayXY,          /// 11
+              pInfo,             /// 12
+              pHitID,            /// 13
+              pEmpty };          /// 14
 
 //======================================== User Setting
 
-const int Div[2] = {3,2}; /// x,y
-plotID canvas[6] = { pEZ, pRecoilXY, pThetaCM,
-                     pThetaCM_Z, pExCal, pInfo};
+const int Div[2] = {4,2}; /// x,y
+plotID canvas[8] = { pEZ, pRecoilXY, pRecoilXY1, pRecoilXY2,
+                     pThetaCM_Z, pExCal, pInfo, pArrayXY};
 
 //TString gate = "hit == 1 && rhoRecoil > 10 && rhoElum1 > 72.6 && loop == 1";
 //TString gate = "hit == 1 && loop <= 1 && rhoRecoil > 10 ";
@@ -54,6 +57,7 @@ bool shownKELines = false;
 double * FindRange(TString branch, TString gate, TTree * tree, double output[2]);
 vector<string> SplitStr(string tempLine, string splitter, int shift = 0);
 void Plot(plotID pID);
+double ExtractNumber(int index, TMacro * macro);
 
 TFile * file;
 TTree * tree; 
@@ -62,10 +66,13 @@ TString msg2;
 TObjArray * fxList;
 TObjArray * txList;
 
+double BfieldTheta = 0;
 double length = 50.5;
 double firstPos = -110;
 double rhoRecoil = 50;
 double posRecoil = 400;
+double posRecoil1 = 400;
+double posRecoil2 = 400;
 
 double eRange[2] = {0, 10};
 double recoilERange[2];
@@ -81,12 +88,6 @@ void Check_Simulation(TString filename = "transfer.root", Int_t padSize = 300){
   TMacro * reactionConfig = (TMacro *) file->FindObjectAny("reactionConfig");
   Reaction=reactionConfig->GetName(); ///TODO change to Latex
   
-  TMacro * detGeo = (TMacro *) file->FindObjectAny("detGeo");
-  TString field = detGeo->GetListOfLines()->At(0)->GetName();
-  int pp = field.First('/');
-  field.Remove(pp);
-  msg2.Form("field = %.2f T", field.Atof());
-
   fxList = (TObjArray *) file->FindObjectAny("fxList");
   txList = (TObjArray *) file->FindObjectAny("txList");
 
@@ -100,72 +101,59 @@ void Check_Simulation(TString filename = "transfer.root", Int_t padSize = 300){
    gStyle->SetLabelSize(0.05, "XY");
    gStyle->SetTitleFontSize(0.1);
 
-
 //========================================= detector Geometry
-   string detGeoFileName = "detectorGeo.txt";
-   int numDet;
-   int rDet = 5; /// number of detector at different position, row-Det
-   int cDet = 6; /// number of detector at same position, column-Det
-   vector<double> pos;
+  //================================== detetcor Geometry
+  printf("=================================\n");
+  printf(" loading detector Geometry.\n");
+  TMacro * detGeo = (TMacro *) file->FindObjectAny("detGeo");  
+  double field = ExtractNumber(0, detGeo);
+  TString fdmsg = field > 0 ? "out of plan" : "into plan";
+  msg2.Form("field = %.2f T, %s", field, fdmsg.Data());
+  
+  length = ExtractNumber(5, detGeo);
+  
+  posRecoil  = ExtractNumber(6, detGeo);
+  rhoRecoil  = ExtractNumber(7, detGeo);
+  posRecoil1 = ExtractNumber(9, detGeo);
+  posRecoil2 = ExtractNumber(10, detGeo);
+  firstPos   = ExtractNumber(14, detGeo);  
    
-   printf("----- loading detector geometery : %s.", detGeoFileName.c_str());
-   ifstream detFile;
-   detFile.open(detGeoFileName.c_str());
-   int i = 0;
-   if( detFile.is_open() ){
-      string x;
-      while( detFile >> x){
-         ///printf("%d, %s \n", i,  x.c_str());
-         if( x.substr(0,2) == "//" )  continue;
-         if( i == 5 ) length   = atof(x.c_str());
-         if( i == 6 ) posRecoil = atof(x.c_str());
-         if( i == 7 ) rhoRecoil = atof(x.c_str());
-         if( i == 14 ) firstPos = atof(x.c_str());
-         if( i == 17 ) cDet = atoi(x.c_str());
-         if( i >= 18 ) {
-            pos.push_back(atof(x.c_str()));
-         }
-         i = i + 1;
-      }
-      
-      rDet = pos.size();
-      detFile.close();
-      printf("... done.\n");
-      
-      vector<double> posTemp = pos;
-      for(int id = 0; id < rDet; id++){
-        if( firstPos > 0 ) pos[id] = firstPos + posTemp[id];
-        if( firstPos < 0 ) pos[id] = firstPos - posTemp[rDet -1 - id];
-      }
-      
-      for(int i = 0; i < rDet ; i++){
-         if( firstPos > 0 ){
-            printf("%d, %10.2f mm - %10.2f mm \n", i, pos[i], pos[i] + length);
-         }else{
-            printf("%d, %10.2f mm - %10.2f mm \n", i, pos[i] - length , pos[i]);
-         }
-      }
-      printf("=======================\n");
+  vector<double> pos;
+  int nLine = detGeo->GetListOfLines()->GetLast() + 1;
+  for( int i = 17; i < nLine; i++){
+    pos.push_back(ExtractNumber(i, detGeo));
+  }
+  int rDet = pos.size(); /// number of detector at different position, row-Det
+  int cDet = (int) ExtractNumber(16, detGeo); /// number of detector at same position, column-Det
 
-          
-   }else{
-       printf("... fail\n");
-       return;
-   }
-   
-   numDet = rDet * cDet;
-   
-   if( firstPos > 0 ){
-      zRange[1] = pos[0]-50;
-      zRange[2] = pos[rDet-1] + length + 50;
-   }else{
-      zRange[1] = pos[0]- length - 50;
-      zRange[2] = pos[rDet-1] + 50;
-   }
-   
-   printf(" zRange : %f - %f \n", zRange[1], zRange[2]);
-   printf("=================================\n");
-   
+  vector<double> posTemp = pos;
+  for(int id = 0; id < rDet; id++){
+    if( firstPos > 0 ) pos[id] = firstPos + posTemp[id];
+    if( firstPos < 0 ) pos[id] = firstPos - posTemp[rDet -1 - id];
+  }
+  
+  for(int i = 0; i < rDet ; i++){
+     if( firstPos > 0 ){
+        printf("%d, %10.2f mm - %10.2f mm \n", i, pos[i], pos[i] + length);
+     }else{
+        printf("%d, %10.2f mm - %10.2f mm \n", i, pos[i] - length , pos[i]);
+     }
+  }
+  printf("=================================\n");
+  
+  int numDet = rDet * cDet;
+
+  if( firstPos > 0 ){
+    zRange[1] = pos[0]-50;
+    zRange[2] = pos[rDet-1] + length + 50;
+  }else{
+    zRange[1] = pos[0]- length - 50;
+    zRange[2] = pos[rDet-1] + 50;
+  }
+
+  printf(" zRange : %f - %f \n", zRange[1], zRange[2]);
+  printf("=================================\n");
+  
    //========================================= Ex List;
    printf(" loading Ex list\n");
    
@@ -189,44 +177,28 @@ void Check_Simulation(TString filename = "transfer.root", Int_t padSize = 300){
    }
    
    printf("=================================\n");
-   
+  
    //========================================= reaction parameters
-   printf(" loading reaction parameters");
-   ifstream reactionfile;
-   reactionfile.open("reaction.dat");
-   double mass, q, beta, Et, massB;
-   double alpha, gamm, slope;
    
-   if( reactionfile.is_open() ){
-      string x;
-      int i = 0;
-      while( reactionfile >> x ){
-         if( x.substr(0,2) == "//" )  continue;
-         if( i == 0 ) mass = atof(x.c_str());
-         if( i == 1 ) q    = atof(x.c_str());
-         if( i == 2 ) beta = atof(x.c_str()); 
-         if( i == 3 ) Et   = atof(x.c_str()); 
-         if( i == 4 ) massB = atof(x.c_str()); 
-         if( i == 5 ) alpha = atof(x.c_str()); 
-         i = i + 1;
-      }
-      printf("... done.\n");
-
-      gamm = 1./TMath::Sqrt(1-beta*beta);
-      slope = alpha * beta;
-      printf("\tmass-b    : %f MeV/c2 \n", mass);
-      printf("\tcharge-b  : %f \n", q);
-      printf("\tE-total   : %f MeV \n", Et);
-      printf("\tmass-B    : %f MeV/c2 \n", massB);		 
-      printf("\tbeta      : %f \n", beta);
-      printf("\tslope     : %f MeV/mm \n", slope);
-      printf("=================================\n");
-
-   }else{
-      printf("... fail.\n");
-   }
-   reactionfile.close();
+   printf(" loading reaction parameters \n");
+   TMacro * reactionData = (TMacro *) file->FindObjectAny("reactionData");
    
+   double mass  = ExtractNumber(0, reactionData);
+   double q     = ExtractNumber(1, reactionData);
+   double beta  = ExtractNumber(2, reactionData);
+   double Et    = ExtractNumber(3, reactionData);
+   double massB = ExtractNumber(4, reactionData);
+   double alpha = ExtractNumber(5, reactionData);
+   
+   double gamm = 1./TMath::Sqrt(1-beta*beta);
+   double slope = alpha * beta;
+   printf("\tmass-b    : %f MeV/c2 \n", mass);
+   printf("\tcharge-b  : %f \n", q);
+   printf("\tE-total   : %f MeV \n", Et);
+   printf("\tmass-B    : %f MeV/c2 \n", massB);		 
+   printf("\tbeta      : %f \n", beta);
+   printf("\tslope     : %f MeV/mm \n", slope);
+   printf("=================================\n");
    
    //=================================== calculate Ranges
    
@@ -319,6 +291,16 @@ vector<string> SplitStr(string tempLine, string splitter, int shift = 0){
   return output;
 }
 
+double ExtractNumber(int index, TMacro * macro){
+  
+  TString field = macro->GetListOfLines()->At(index)->GetName();
+  
+  field.Remove(field.First('/'));
+  
+  return field.Atof();
+  
+}
+
 void Plot(plotID pID) {
    
    if( pID == pEZ){
@@ -333,6 +315,14 @@ void Plot(plotID pID) {
    if( pID == pRecoilXY       ){
       TH2F * hRecoilXY = new TH2F("hRecoilXY", Form("RecoilXY [gated] @ %4.0f mm; X [mm]; Y [mm]", posRecoil ), 400, -rhoRecoil, rhoRecoil, 400,-rhoRecoil, rhoRecoil);
       tree->Draw("yRecoil:xRecoil>>hRecoilXY", gate, "colz");
+   }
+   if( pID == pRecoilXY1       ){
+      TH2F * hRecoilXY1 = new TH2F("hRecoilXY1", Form("RecoilXY-1 [gated] @ %4.0f mm; X [mm]; Y [mm]", posRecoil1 ), 400, -rhoRecoil, rhoRecoil, 400,-rhoRecoil, rhoRecoil);
+      tree->Draw("yRecoil1:xRecoil1>>hRecoilXY1", gate, "colz");
+   }
+   if( pID == pRecoilXY2       ){
+      TH2F * hRecoilXY2 = new TH2F("hRecoilXY2", Form("RecoilXY-2 [gated] @ %4.0f mm; X [mm]; Y [mm]", posRecoil2 ), 400, -rhoRecoil, rhoRecoil, 400,-rhoRecoil, rhoRecoil);
+      tree->Draw("yRecoil2:xRecoil2>>hRecoilXY2", gate, "colz");
    }
    if( pID == pRecoilRZ       ){
       TH2F * hRecoilRZ = new TH2F("hRecoilRZ", "RecoilR - Z [gated]; z [mm]; RecoilR [mm]",  zRange[0], zRange[1], zRange[2], 400,0, rhoRecoil);
@@ -392,6 +382,10 @@ void Plot(plotID pID) {
    if( pID == pRecoilRThetaCM ){
      TH2F * hRecoilRThetaCM = new TH2F("hRecoilRThetaCM", "RecoilR - thetaCM [gated]; thetaCM [deg]; RecoilR [mm]", 400, 0, 60, 400,0, rhoRecoil);
      tree->Draw("rhoRecoil:thetaCM>>hRecoilRThetaCM", gate, "colz");
+   }
+   if( pID == pArrayXY ){
+     TH2F * hArrayXY = new TH2F("hArrayXY", "Array-XY [gated]; X [mm]; Y [mm]", 400, -20, 20, 400, -20, 20);
+     tree->Draw("yArray:xArray>>hArrayXY", gate, "colz");
    }
    if( pID == pInfo           ){
       TLatex text;
