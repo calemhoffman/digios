09d86ad0e38a21ed9154d921a5c60102338c40e5
HELIOS_Mac <helios_mac@anl.gov>
2021-08-27 16:14:49 -0500

    The GeneralSortMapping.h will copy to ~/.proof/ for  GeneralSortTraceProof, no need to coorect path for all machine

--- analysis/Armory/process_run.C
+++ analysis/Armory/process_run.C
@@ -4,19 +4,18 @@
 #include <TFile.h>
 #include <TProof.h>
 
-void process_run(Int_t RUNNUM=5, int isTrace=0, Int_t SORTNUM=0){
+void process_run(Int_t RUNNUM=5, int isTrace=0){
   
-  if (SORTNUM==0) {
     TString name;
     name.Form("../root_data/run%03d.root", RUNNUM);
     TFile f(name);
 
     if( !f.IsOpen() ) return;
-    
+
     TTree *t1 = (TTree*)f.Get("tree");
 
     isTrace = abs(isTrace);
-    
+
     TString processCmd;
     if( isTrace == 1 ) {
         processCmd.Form("../Armory/GeneralSort.C++");
@@ -27,11 +26,12 @@ void process_run(Int_t RUNNUM=5, int isTrace=0, Int_t SORTNUM=0){
         //t1->Process(processCmd);
         //f.Close();
     }else if(isTrace > 1){
+        
         TChain * chain = new TChain("tree");
         chain->Add(name); 
-   
+
         TProof * p = TProof::Open("", Form("workers=%d", isTrace));
-   
+        p->ShowCache(); // this should be ~/proof/....
         chain->SetProof();
         chain->Process("../Armory/GeneralSortTraceProof.C++", name);
                 
@@ -41,7 +41,7 @@ void process_run(Int_t RUNNUM=5, int isTrace=0, Int_t SORTNUM=0){
     }else{
         return;
     }
-    
+
     printf("======================================done RUN-%03d \n", RUNNUM);
 
     if( isTrace < 2 ){
@@ -52,12 +52,6 @@ void process_run(Int_t RUNNUM=5, int isTrace=0, Int_t SORTNUM=0){
     printf("%s \n", name.Data());
     printf("============================================= \n");
     //gROOT->ProcessLine(".q");
-    
-  }else if (SORTNUM==1) {
-    TString name("gen.root");
-    TFile ff(name);
-    TTree *t2 = (TTree*)ff.Get("gen_tree");
-    t2->Process("../codes/Monitors.C++");
-    ff.Close();
-  }
+
+
 }
