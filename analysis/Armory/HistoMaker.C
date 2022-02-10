561e156c7fdd56a1dc5380137d637410d7707188
Calem Hoffman <calem.hoffman@gmail.com>
2022-02-10 15:30:14 -0600

    HistoMaker Updates

--- analysis/Armory/HistoMaker.C
+++ analysis/Armory/HistoMaker.C
@@ -5,48 +5,12 @@ void HistoMaker(TString rootFile = "root_data/s005_32Si_trace_run135.root") {
    const char* treeName="tree";
    TFile *file0 = new TFile (rootFile, "read"); 
    TTree *tree = (TTree*)file0->Get(treeName);
-   //general
-   tree->SetBranchAddress("eventID",&eventID);
-   tree->SetBranchAddress("run",&run);
-   //array
-   tree->SetBranchAddress("multiHit",&multiHit);
-   tree->SetBranchAddress("e",e);
-   tree->SetBranchAddress("x",x);
-   tree->SetBranchAddress("xf",xf);
-   tree->SetBranchAddress("xn",xn);
-   tree->SetBranchAddress("te",te);
-   tree->SetBranchAddress("te_t",te_t);
-   tree->SetBranchAddress("te_r",te_r);
-   tree->SetBranchAddress("z",z);
-   //recoil
-   tree->SetBranchAddress("rdt",rdt);
 
-   //timing
+   setTreeBranches(tree);
+   defineHistos();
+   getCalibrations();
 
-   //histograms
-   //general
-   TH1F * heventID = new TH1F("heventID","heventID",1000,0,1e6);
-   TH1F * hrunID = new TH1F("hrunID","hrunID",200,0,200);
-
-   //array
-   TH2F * hxfxn[24];
-   TH2F * hesumx[24];
-   TH2F * htetx[24];
-   TH2F * htetr[24];
-   TH2F * htete[24];
-   for (int i=0;i<24;i++) {
-      hxfxn[i] = new TH2F(Form("hxfxn%d",i),Form("hxfxn%d; xf; xn",i), 1000,0,2500,1000,0,2500);
-      hesumx[i] = new TH2F(Form("hesumx%d",i),Form("hesumx%d; e; xf+xn",i), 1000,0,10,1000,0,2500);
-      htetx[i] = new TH2F(Form("htetx%d",i),Form("htetx%d; x; te_t",i), 1000,-1.1,1.1,350,90,125);
-      htetr[i] = new TH2F(Form("htetr%d",i),Form("htetr%d; te_r; te_t",i), 1000,0,10,350,90,125);
-      htete[i] = new TH2F(Form("htete%d",i),Form("htete%d; e; te_t",i), 1000,0,10,350,90,125);
-   }
-   //recoil
-   //ez
-   TH2F * hez = new TH2F("hez","hez; z; e",1000,-600,-100,1000,0,10);
-   TH2F * hezg = new TH2F("hezg","gated hez; z; e",1000,-600,-100,1000,0,10);
-   //timing
-
+   //Process events
    Int_t nEntries = tree->GetEntries();
    printf("nEntries: %d\n",nEntries);
 
@@ -65,22 +29,31 @@ void HistoMaker(TString rootFile = "root_data/s005_32Si_trace_run135.root") {
       for (int i=0;i<multiHit;i++) {
          for (int detID=0;detID<24;detID++) {
             if ( TMath::IsNaN(e[detID]) || e[detID]<0.5 ) continue;
-            //"no" gates
+            //process
+            te_t[detID] = te_t[detID] - te_r[detID]; //correct time
+
+            //fill //"no" gates
             hxfxn[detID]->Fill(xf[detID],xn[detID]);
             hesumx[detID]->Fill(e[detID],xf[detID]+xn[detID]);
             htetx[detID]->Fill(x[detID],te_t[detID]);
             htetr[detID]->Fill(te_r[detID],te_t[detID]);
             htete[detID]->Fill(e[detID],te_t[detID]);
+            htet[detID]->Fill(te_t[detID]);
             hez->Fill(z[detID],e[detID]);
 
             //full gates
-            if (rdt[4] > 500) { //de back needs a signal
+            if (  (rdt[4] > 500) && //rdt energy
+                  (x[detID]>-0.9 && x[detID]<0.9) // x
+               )
+            { //de back needs a signal
                hezg->Fill(z[detID],e[detID]);
+               htetg[detID]->Fill(te_t[detID]);
             } //full gates
          } //detID
       } //multiHit
    } //entryNumber
 
+//WRITE FILE
    TFile * outFile = new TFile("HistMaker.root","RECREATE");
    // outFile->mkdir("array");
    hez->Write(); hezg->Write();
@@ -88,18 +61,21 @@ void HistoMaker(TString rootFile = "root_data/s005_32Si_trace_run135.root") {
    for (int detID=0;detID<24;detID++) {
       hxfxn[detID]->Write(); hesumx[detID]->Write();
       htetx[detID]->Write(); htete[detID]->Write();
-      htetr[detID]->Write();
+      htetr[detID]->Write(); htet[detID]->Write();
    }
+   cali_tetX.Write("correction_tetX");
 
+//DRAW
    TCanvas *ctetx = new TCanvas("ctetx","ctetx",1000,800);
+   TCanvas *ctete = new TCanvas("ctete","ctete",1000,800);
+   TCanvas *ctet = new TCanvas("ctet","ctet",1000,800);
    ctetx->Divide(6,4);
+   ctete->Divide(6,4);
+   ctet->Divide(6,4);
    for (int detID=0;detID<24;detID++) {
       ctetx->cd(detID+1); htetx[detID]->Draw("colz");
-   }
-   TCanvas *ctete = new TCanvas("ctete","ctete",1000,800);
-   ctete->Divide(6,4);
-   for (int detID=0;detID<24;detID++) {
       ctete->cd(detID+1); htete[detID]->Draw("colz");
+      ctet->cd(detID+1); htetg[detID]->Draw("colz");
    }
 
    outFile->Write(); outFile->Close();
--- analysis/Armory/HistoMaker.h
+++ analysis/Armory/HistoMaker.h
@@ -75,3 +75,77 @@ Float_t         trdt[8];
 Float_t         trdt_t[8];
 Float_t         trdt_r[8];
 
+void setTreeBranches(TTree * tree) {
+   //general
+   tree->SetBranchAddress("eventID",&eventID);
+   tree->SetBranchAddress("run",&run);
+   //array
+   tree->SetBranchAddress("multiHit",&multiHit);
+   tree->SetBranchAddress("e",e);
+   tree->SetBranchAddress("x",x);
+   tree->SetBranchAddress("xf",xf);
+   tree->SetBranchAddress("xn",xn);
+   tree->SetBranchAddress("te",te);
+   tree->SetBranchAddress("te_t",te_t);
+   tree->SetBranchAddress("te_r",te_r);
+   tree->SetBranchAddress("z",z);
+   //recoil
+   tree->SetBranchAddress("rdt",rdt); 
+}
+
+TH1F * heventID;
+TH1F * hrunID;
+TH2F * hxfxn[24];
+TH2F * hesumx[24];
+TH2F * htetx[24];
+TH2F * htetr[24];
+TH2F * htete[24];
+TH1F * htet[24];
+TH1F * htetg[24];
+TH2F * hez;
+TH2F * hezg;
+void defineHistos() {
+       //histograms
+   //general
+   heventID = new TH1F("heventID","heventID",1000,0,1e6);
+   hrunID = new TH1F("hrunID","hrunID",200,0,200);
+   //array
+   for (int i=0;i<24;i++) {
+        hxfxn[i] = new TH2F(Form("hxfxn%d",i),Form("hxfxn%d; xf; xn",i), 1000,0,2500,1000,0,2500);
+        hesumx[i] = new TH2F(Form("hesumx%d",i),Form("hesumx%d; e; xf+xn",i), 1000,0,10,1000,0,2500);
+        htetx[i] = new TH2F(Form("htetx%d",i),Form("htetx%d; x; te_t",i), 1000,-1.1,1.1,350,90,125);
+        htetr[i] = new TH2F(Form("htetr%d",i),Form("htetr%d; te_r; te_t",i), 1000,0,10,350,90,125);
+        htete[i] = new TH2F(Form("htete%d",i),Form("htete%d; e; te_t",i), 1000,0,10,350,90,125);
+        htet[i] = new TH1F(Form("htet%d",i),Form("htet%d; te_t",i), 350,90,125);
+        htetg[i] = new TH1F(Form("htetg%d",i),Form("htetg%d; te_t",i), 350,90,125);
+   }
+   //recoil
+   //ez
+   hez = new TH2F("hez","hez; z; e",1000,-600,-100,1000,0,10);
+   hezg = new TH2F("hezg","gated hez; z; e",1000,-600,-100,1000,0,10);
+   //timing
+}
+
+Float_t tetXCorr[30][10];
+ifstream file;
+TMacro cali_tetX("correction_tetX.dat");
+void getCalibrations() {
+    file.open("correction_tetX.dat");
+    if( file.is_open() ){
+        printf("loading tetX correction parameters.");    
+        int d;
+        double a0, a1, a2, a3, a4, a5, a6, a7, a8;
+        int i = 0;
+        while( file >>  d >> a0 >> a1 >> a2 >> a3 >> a4 >> a5 >> a6 /* >> a7 >> a8*/){
+            if( i >= 29) break;
+            tetXCorr[i][0] = a0;tetXCorr[i][1] = a1;tetXCorr[i][2] = a2;
+            tetXCorr[i][3] = a3;tetXCorr[i][4] = a4;tetXCorr[i][5] = a5;
+            tetXCorr[i][6] = a6;//tetXCorr[i][7] = a7;tetXCorr[i][8] = a8; 
+            i = i + 1;
+        }
+        printf(".... done.\n");
+        //TMacro cali_tetX("correction_tetX.dat");
+        //cali_tetX.Write("correction_tetX");
+    }
+    file.close();
+}
