61e94bc8aa0f40d0c75f5ff9e994aff9432adf04
Calem Hoffman <calem.hoffman@gmail.com>
2022-02-15 09:30:38 -0600

    Messing with data

--- analysis/Armory/HistoMaker.C
+++ analysis/Armory/HistoMaker.C
@@ -1,8 +1,8 @@
 #include "HistoMaker.h"
 
-void HistoMaker(TString rootFile = "root_data/s005_32Si_trace_run093-157.root") {
+void HistoMaker(TString rootFile = "root_data/s005_32Si_trace_tp.root") {
    //rootFile="root_data/s005_32Si_trace_run093-157.root";
-   rootFile="root_data/s005_32Si_trace_dp.root";
+   //rootFile="root_data/s005_32Si_trace_dp.root";
    const char* treeName="tree";
    TFile *file0 = new TFile (rootFile, "read"); 
    TTree *tree = (TTree*)file0->Get(treeName);
@@ -36,8 +36,8 @@ void HistoMaker(TString rootFile = "root_data/s005_32Si_trace_run093-157.root") 
          for (int detID=0;detID<24;detID++) {
             if ( TMath::IsNaN(e[detID]) || e[detID]<0.5 ) continue;
             //process
-            te_t[detID] = te_t[detID] - te_r[detID]; //correct time
-
+            te_t[detID] = te_t[detID]*10.0 - te_r[detID]*0.0; //correct time
+            dt[detID] = (te_t[detID] - trdt_t[4]*10.0); //de back only
             //fill //"no" gates
             hxfxn[detID]->Fill(xf[detID],xn[detID]);
             hesumx[detID]->Fill(e[detID],xf[detID]+xn[detID]);
@@ -50,6 +50,7 @@ void HistoMaker(TString rootFile = "root_data/s005_32Si_trace_run093-157.root") 
             hez->Fill(z[detID],e[detID]);
             hcoin_t->Fill(tcoin_t);
             hcoinTime->Fill(coinTime);
+            hdt->Fill(dt[detID]);
             hExTot->Fill(Ex);
             for (int recID=0;recID<8;recID++) {
                if ( TMath::IsNaN(rdt[recID]) ) continue;
@@ -64,6 +65,9 @@ void HistoMaker(TString rootFile = "root_data/s005_32Si_trace_run093-157.root") 
                   (x[detID]>-0.9 && x[detID]<0.9) //&& //x
                )
             { //de back needs a signal
+               hdtx[detID]->Fill(x[detID],dt[detID]);
+               hdtr[detID]->Fill(te_r[detID],dt[detID]);
+               hdte[detID]->Fill(e[detID],dt[detID]);
                hezg->Fill(z[detID],e[detID]);
                htetg[detID]->Fill(te_t[detID]);
                heVxg[detID]->Fill(x[detID],e[detID]);
@@ -111,17 +115,23 @@ void HistoMaker(TString rootFile = "root_data/s005_32Si_trace_run093-157.root") 
    TCanvas *ctetx = new TCanvas("ctetx","ctetx",1000,800);
    TCanvas *ctete = new TCanvas("ctete","ctete",1000,800);
    TCanvas *ctet = new TCanvas("ctet","ctet",1000,800);
+   TCanvas *cdtx = new TCanvas("cdtx","cdtx",1000,800);
+   TCanvas *cdte = new TCanvas("cdte","cdte",1000,800);
    TCanvas *cez = new TCanvas("cez","cez",1000,800);
    TCanvas *crdt = new TCanvas("crdt","crdt",1000,800);
    ctetx->Divide(6,4);
    ctete->Divide(6,4);
    ctet->Divide(6,4);
+   cdtx->Divide(6,4);
+   cdte->Divide(6,4);
    cez->Divide(1,2);
    crdt->Divide(2,5);
    for (int detID=0;detID<24;detID++) {
       ctetx->cd(detID+1); htetx[detID]->Draw("colz");
       ctete->cd(detID+1); htete[detID]->Draw("colz");
       ctet->cd(detID+1); htetg[detID]->Draw("colz");
+      cdtx->cd(detID+1); hdtx[detID]->Draw("colz");
+      cdte->cd(detID+1); hdte[detID]->Draw("colz");
    }
    for (int i=0;i<5;i++) {
       crdt->cd(2*i+1);
