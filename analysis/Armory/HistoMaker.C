3c85cb6e81bd2234e536f2f168d99676fde59a46
Calem Hoffman <calem.hoffman@gmail.com>
2022-04-15 17:02:18 -0500

    Bunch fo local stuff
    s005

--- analysis/Armory/HistoMaker.C
+++ analysis/Armory/HistoMaker.C
@@ -1,7 +1,7 @@
 #include "HistoMaker.h"
 
-void HistoMaker(TString rootFile = "root_data/s005_32Si_trace_run135_short.root") {
-   rootFile="root_data/s005_32Si_trace_tp.root";
+void HistoMaker(TString rootFile = "root_data/s005_32Si_tp.root") {
+   //rootFile="root_data/s005_32Si_tp.root";
    //rootFile="root_data/s005_32Si_trace_dp.root";
    const char* treeName="tree";
    TFile *file0 = new TFile (rootFile, "read"); 
@@ -30,53 +30,56 @@ void HistoMaker(TString rootFile = "root_data/s005_32Si_trace_run135_short.root"
             //process
             //correct timing
             Double_t tetXCorrFactor = 0;
-            for (int ipwr=0;ipwr<=8;ipwr++) {
+            for (int ipwr=0;ipwr<8;ipwr++) {
                tetXCorrFactor-= tetXCorr[detID][ipwr]*TMath::Power(x[detID],ipwr);
                //printf("%f %d %f\n",te_t[detID],detID,tetXCorrFactor);
             }
-
+            tetXCorrFactor-=tetXCorr[detID][8];
             //dt[detID] = 10.0*((float)coin_t + te_t[detID] - trdt_t[4]);//coinTimeUC;
             Int_t tsDiff = (int)(e_t[detID] - rdt_t[4]);
             dt[detID] = 10.0*((float)tsDiff + te_t[detID] - trdt_t[4]) + tetXCorrFactor;//coinTimeUC;
-            te_t[detID] = coinTimeUC;
-            //fills no gates
-            hxfxn[detID]->Fill(xf[detID],xn[detID]);
-            hesumx[detID]->Fill(e[detID],xf[detID]+xn[detID]);
-            htetx[detID]->Fill(x[detID],te_t[detID]);
-            htetr[detID]->Fill(te_r[detID],te_t[detID]);
-            htete[detID]->Fill(e[detID],te_t[detID]);
-            htet[detID]->Fill(te_t[detID]);
-            heVx[detID]->Fill(x[detID],e[detID]);
-            hEx[detID]->Fill(Ex);
-            hez->Fill(z[detID],e[detID]);
-            hcoin_t->Fill(tcoin_t);
-            hcoinTime->Fill(coinTimeUC);
-            hdt[detID]->Fill(dt[detID]);
-            hExTot->Fill(Ex);
-            hdtx[detID]->Fill(x[detID],dt[detID]);
-            hdtr[detID]->Fill(te_r[detID],dt[detID]);
-            hdte[detID]->Fill(e[detID],dt[detID]);
-      
-            for (int recID=0;recID<8;recID++) {
-               if ( TMath::IsNaN(rdt[recID]) ) continue;
-               hrdt[recID]->Fill(rdt[recID]);
-               if ( recID < 5 ) {
-                  hrdt2d[recID]->Fill(rdt[6],rdt[recID]);
-                  hrdt2d[recID+5]->Fill(rdt[7],rdt[recID]);
+            //dt[detID] -= (rdt[4]-565.)*0.0413;
+            //te_t[detID] = coinTimeUC;
+            //fills only x range gate
+            if (x[detID]>-0.95 && x[detID]<0.95) {
+               hxfxn[detID]->Fill(xf[detID],xn[detID]);
+               hesumx[detID]->Fill(e[detID],xf[detID]+xn[detID]);
+               htetx[detID]->Fill(x[detID],te_t[detID]);
+               htetr[detID]->Fill(te_r[detID],te_t[detID]);
+               htete[detID]->Fill(e[detID],te_t[detID]);
+               htet[detID]->Fill(te_t[detID]);
+               heVx[detID]->Fill(x[detID],e[detID]);
+               hEx[detID]->Fill(Ex);
+               hez->Fill(z[detID],e[detID]);
+               hcoin_t->Fill(tcoin_t);
+               hcoinTime->Fill(coinTimeUC);
+               hdt[detID]->Fill(dt[detID]);
+               hExTot->Fill(Ex);
+               hdtx[detID]->Fill(x[detID],dt[detID]);
+               hdtr[detID]->Fill(te_r[detID],dt[detID]);
+               hdte[detID]->Fill(e[detID],dt[detID]);
+               hdtEx[detID]->Fill(Ex,dt[detID]);
+               hdtrdt[detID]->Fill(rdt[7],dt[detID]);
+               for (int recID=0;recID<8;recID++) {
+                  if ( TMath::IsNaN(rdt[recID]) ) continue;
+                  hrdt[recID]->Fill(rdt[recID]);
+                  if ( recID < 5 ) {
+                     hrdt2d[recID]->Fill(rdt[6],rdt[recID]);
+                     hrdt2d[recID+5]->Fill(rdt[7],rdt[recID]);
+                  }
                }
             }
             //full gates
-            if (  (rdt[4] > 500) && //rdt energy
-                  (x[detID]>-0.9 && x[detID]<0.9) && //x
-                  (rdt[0] > 500 && rdt[0] < 1700) && //recoil energy
-                  (dt[detID] < -10.0 && dt[detID]>-33.0)
-               )
+            if (  (rdt[4] > 500 && rdt[7]>50 && rdt[4] <1800 && rdt[7]<1000) && //rdt energy
+                  (x[detID]>-0.95 && x[detID]<0.95) && //x
+                  (dt[detID]>-20 && dt[detID]<0)               )
             { //de back needs a signal
                hezg->Fill(z[detID],e[detID]);
                htetg[detID]->Fill(te_t[detID]);
                heVxg[detID]->Fill(x[detID],e[detID]);
                hExg[detID]->Fill(Ex);
                hExgTot->Fill(Ex);
+               hdtg[detID]->Fill(dt[detID]);
                for (int recID=0;recID<8;recID++) {
                   if ( TMath::IsNaN(rdt[recID]) ) continue;
                   hrdtg[recID]->Fill(rdt[recID]);
@@ -101,7 +104,9 @@ void HistoMaker(TString rootFile = "root_data/s005_32Si_trace_run135_short.root"
       htetr[detID]->Write(); htet[detID]->Write();
       heVx[detID]->Write(); heVxg[detID]->Write();
       hEx[detID]->Write(); hExg[detID]->Write();
-      hdt[detID]->Write();
+      hdt[detID]->Write(); hdtr[detID]->Write();
+      hdtx[detID]->Write();hdte[detID]->Write();
+      hdtrdt[detID]->Write();
    }
    hcoin_t->Write(); hcoinTime->Write();
    hExTot->Write(); hExgTot->Write();
@@ -117,40 +122,25 @@ void HistoMaker(TString rootFile = "root_data/s005_32Si_trace_run135_short.root"
    cali_tetX.Write("correction_tetX");
 
 //DRAW
-   TCanvas *ctetx = new TCanvas("ctetx","ctetx",1000,800);
-   TCanvas *ctete = new TCanvas("ctete","ctete",1000,800);
-   TCanvas *ctetr = new TCanvas("ctetr","ctetr",1000,800);
-   TCanvas *ctet = new TCanvas("ctet","ctet",1000,800);
+   TCanvas *cdtr = new TCanvas("cdtr","cdtr",1000,800);
    TCanvas *cdtx = new TCanvas("cdtx","cdtx",1000,800);
    TCanvas *cdte = new TCanvas("cdte","cdte",1000,800);
    TCanvas *cez = new TCanvas("cez","cez",1000,800);
-   TCanvas *crdt = new TCanvas("crdt","crdt",1000,800);
-   ctetx->Divide(6,4);
-   ctete->Divide(6,4);
-   ctetr->Divide(6,4);
-   ctet->Divide(6,4);
+   cdtr->Divide(6,4);
    cdtx->Divide(6,4);
    cdte->Divide(6,4);
    cez->Divide(1,2);
-   crdt->Divide(2,5);
    for (int detID=0;detID<24;detID++) {
-      ctetx->cd(detID+1); htetx[detID]->Draw("");
-      ctete->cd(detID+1); htete[detID]->Draw("");
-      ctetr->cd(detID+1); htetr[detID]->Draw("");
-      ctet->cd(detID+1); htetg[detID]->Draw("");
+     cdtr->cd(detID+1); hdtrdt[detID]->Draw("");
       cdtx->cd(detID+1); hdtx[detID]->Draw("");
-      cdte->cd(detID+1); hdte[detID]->Draw("");
+      cdte->cd(detID+1); hdt[detID]->Draw("");hdtg[detID]->Draw("same");
    }
-   for (int i=0;i<5;i++) {
-      crdt->cd(2*i+1);
-      hrdt2dg[i]->Draw("colz");
-      crdt->cd(2*i+2);
-      hrdt2dg[i+5]->Draw("colz");
-   }
+
    cez->cd(1); hez->Draw("colz");
-   cez->cd(2); hezg->Draw("colz");
+   cez->cd(2); hezg->SetMarkerStyle(7);hezg->Draw("");
    outFile->Write(); outFile->Close();
 
    TCanvas *cc = new TCanvas("cc","cc",800,800);
    cc->Clear(); cc->cd();
+   hExgTot->Draw();
 } //end
