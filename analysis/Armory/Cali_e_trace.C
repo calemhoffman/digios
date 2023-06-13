8ddef0803c71ccb5b078dcaa1b2e23a85020045a
calemhoffman <calem.hoffman@gmail.com>
2023-06-13 14:20:19 -0500

    Update Cali_e_trace.C

--- analysis/Armory/Cali_e_trace.C
+++ analysis/Armory/Cali_e_trace.C
@@ -213,10 +213,10 @@ Bool_t Cali_e_trace::Process(Long64_t entry){
      ///========= calculate x, range (-1,1)    //crh otherwise check...
      if( hitID[idet] == 3 ) {
       // x[idet] = (xfC[idet]-xnC[idet])/(xfC[idet]+xnC[idet]);
-      if (xfC[idet] >= e[idet[/2.) {
+      if (xfC[idet] >= e[idet]/2.) {
          hitID[idet] = 1;
       } 
-      if (xnC[idet] >= e[idet[/2.) {
+      if (xnC[idet] >= e[idet]/2.) {
          hitID[idet] = 2;
       }
      }
