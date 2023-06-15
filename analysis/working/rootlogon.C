8df00e9f21aa2aa87a1299455cc7577e1888c76a
calemhoffman <calem.hoffman@gmail.com>
2023-06-15 10:48:20 -0500

    calibrations

--- analysis/working/rootlogon.C
+++ analysis/working/rootlogon.C
@@ -3,28 +3,25 @@
   gROOT->ProcessLine(".L ../Armory/AutoFit.C");
   //printf("GSUtil_new_cc.so loaded\n");
   gROOT->ProcessLine(".L ../Armory/Check_e_x.C");
-  // gROOT->ProcessLine(".L ../Armory/Check_e_z.C");
+  gROOT->ProcessLine(".L ../Armory/Check_e_z.C");
   // gROOT->ProcessLine(".L ../Armory/Check_alignment.C");
   // gROOT->ProcessLine(".L ../Armory/Check_caliResult.C");
   // gROOT->ProcessLine(".L ../Armory/CRH_cals.C");
 
 
   // Set ROOT defaults
-  gStyle->SetPalette(1);
-  gStyle->SetOptStat("im");
-  gStyle->ToggleEditor();
-  gStyle->ToggleToolBar();
+    gROOT->SetStyle("Plain");                       // plain histogram style
   /* gStyle->SetLineWidth(2); */
   /* gStyle->SetStatW(0.3); */
   /* gStyle->SetStatH(0.3); */
   /* gStyle->SetTitleW(0.4); */
   /* gStyle->SetTitleH(0.1); */
-  /* gStyle->SetCanvasBorderMode(0); */
+  gStyle->SetCanvasBorderMode(0);
   /* gStyle->SetPadBorderMode(0); */
   /* gStyle->SetPadColor(0); */
   /* gStyle->SetFrameBorderMode(0); */
   /* gStyle->SetFrameLineWidth(2); */
-  /* gStyle->SetCanvasColor(0); */
+  gStyle->SetCanvasColor(0);
   /* gStyle->SetOptDate(4); */
   /* gStyle->GetAttDate()->SetTextFont(62); */
   /* gStyle->GetAttDate()->SetTextSize(0.02); */
@@ -34,14 +31,15 @@
   gStyle->SetDateX(0);
   gStyle->SetDateY(0);
 
-  gROOT->SetStyle("Plain");                       // plain histogram style
-  gStyle->SetOptStat("nemruoi");                  // expanded stats box
+  gStyle->SetOptStat("nemruoi");
+  gStyle->ToggleEditor();
+  gStyle->ToggleToolBar();                  
   gStyle->SetPadTickX(1);                         // tic marks on all axes
   gStyle->SetPadTickY(1);                         //
   gStyle->SetOptFit(1111);                        // the results of the fits
   gStyle->SetPadGridX(kTRUE);                     // draw horizontal and vertical grids
   gStyle->SetPadGridY(kTRUE);
-  gStyle->SetPalette(108);                        // pretty and useful palette
+  gStyle->SetPalette(109);     //108, 109, 62 , 84                  // pretty and useful palette
   gStyle->SetHistLineWidth(1);                    // a thicker histogram line
   gStyle->SetFrameFillColor(10);                  // a different frame colour
   gStyle->SetTitleFillColor(33);                 // title colour to highlight it
