43ab99a8b1ab5f095ee634cd88852879daa4c447
calemhoffman <calem.hoffman@gmail.com>
2023-08-09 15:23:40 -0500

    Update RDTFit.cpp

--- analysis/Armory/draws/RDTFit.cpp
+++ analysis/Armory/draws/RDTFit.cpp
@@ -2,7 +2,7 @@
 TFile *f[200];
 TCanvas *cc0;
 cc0 = new TCanvas("cc0","rdt0",2200,1400);
-cc0->Clear(); cc0->Divide(2,2); cc0->cd();
+cc0->Clear(); cc0->Divide(2,4); cc0->cd();
 // TCanvas *cc1 = new TCanvas("cc1","rdt1",2200,1400);
 // cc1->Clear(); cc1->Divide(6,5); cc1->cd();
 // TCanvas *cc2 = new TCanvas("cc2","rdt2",2200,1400);
@@ -15,22 +15,26 @@ cc0->Clear(); cc0->Divide(2,2); cc0->cd();
 //                   163,165,167,169,171,
 //                   173,175,177,181,183,
 //                   185,187,189,191,195};
-int runList[2] = {133,175};
-for (Int_t run=0;run<2;run++) {
+int runList[2] = {133};
+for (Int_t run=0;run<1;run++) {
     //cc[runList[run]] = new TCanvas(Form("cc%d",run),Form("rdt%d",run));
-    f[runList[run]] = new TFile(Form("../root_data/h066_31Si_trace_run%d.root",runList[run]));
+    f[runList[run]] = new TFile(Form("h066_31Si_trace_run%d.root",runList[run]));
     TTree *tree = (TTree*)f[runList[run]]->Get("tree");
     TH2F *hrdt[4];
     TH1F *hrdtDE[4];
     TH1F *hrdtE[4];
     TH1F *hrdtTOTE[4];
+    TH1F *hrdtTOTE2[4];
     for (Int_t i=0;i<4;i++) {
-        cc0->cd(i+1);
         hrdtTOTE[i] = new TH1F(Form("hrdtTOTE%d",i),Form("hrdtTOTE%d_%d",i,runList[run]),1000,1000,5600);
+        hrdtTOTE2[i] = new TH1F(Form("hrdtTOTE2%d",i),Form("hrdtTOTE2%d_%d",i,runList[run]),1000,1000,5600);
             TString draw_string(Form("rdt[%d]+rdt[%d]>>hrdtTOTE%d",2*i+1,2*i,i));
-            if (run<1)
-            tree->Draw(draw_string,"","");
-            if (run>0) tree->Draw(draw_string,"","same");
+            TString draw_string2(Form("trdt[%d]+trdt[%d]>>hrdtTOTE2%d",2*i+1,2*i,i));
+            cc0->cd(i+1);
+            if (run<1)   tree->Draw(draw_string,"","");
+            cc0->cd(4*i+1);
+            if (run < 1) tree->Draw(draw_string2,"","");
+            // if (run>0) tree->Draw(draw_string,"","same");
             // fit2GaussP1(hrdtTOTE0,4250,40,3650,40,3900,4500);
     }
 }
