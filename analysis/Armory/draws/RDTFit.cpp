d0cd9dd684ad01223551d77487dca4d7c3c58952
calemhoffman <calem.hoffman@gmail.com>
2023-08-09 15:36:26 -0500

    Update RDTFit.cpp

--- analysis/Armory/draws/RDTFit.cpp
+++ analysis/Armory/draws/RDTFit.cpp
@@ -27,8 +27,8 @@ for (Int_t run=0;run<1;run++) {
     TH1F *hrdtTOTE[4];
     TH1F *hrdtTOTE2[4];
     for (Int_t i=0;i<4;i++) {
-        hrdtTOTE[i] = new TH1F(Form("hrdtTOTE%d",i),Form("hrdtTOTE%d_%d",i,runList[run]),1000,3650,4800);
-        hrdtTOTE2[i] = new TH1F(Form("hrdtTOTE2%d",i),Form("hrdtTOTE2%d_%d",i,runList[run]),1000,3650,4800);
+        hrdtTOTE[i] = new TH1F(Form("hrdtTOTE%d",i),Form("hrdtTOTE%d_%d",i,runList[run]),800,3650,4900);
+        hrdtTOTE2[i] = new TH1F(Form("hrdtTOTE2%d",i),Form("hrdtTOTE2%d_%d",i,runList[run]),800,3650,4900);
             TString draw_string(Form("rdt[%d]+rdt[%d]>>hrdtTOTE%d",2*i+1,2*i,i));
             TString draw_string2(Form("trdt[%d]+trdt[%d]>>hrdtTOTE2%d",2*i+1,2*i,i));
             cc0->cd(i+1);
