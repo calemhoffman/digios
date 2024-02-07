3f0f4d06d84824e3b95f0f9cf1951fb1d1ac01be
calemhoffman <calem.hoffman@gmail.com>
2024-02-07 15:25:16 -0600

    before switch to 32Si angs

--- analysis/working/angdists.py
+++ analysis/working/angdists.py
@@ -20,6 +20,7 @@ pio.templates["mycolor"] = go.layout.Template(layout_colorway=color)
 pio.templates.default = "mycolor"
 stateID=0
 stateID_1=0
+stateID_2=0
 def spfit(x, fitc2s):
 	return fitc2s*splev(x, tck[stateID], der=0)
 def spfit2(x, fitc2s1,fitc2s2):
@@ -29,7 +30,7 @@ for i in range(len(color)):
     fig.add_trace(go.Scatter(x=[i],y=[i],marker_color=color[i],marker_size=20))
 fig.show()
 # %%
-data_file_name='angdist_feb6.csv'
+data_file_name='angdist_feb7.csv'
 df_data = pd.read_csv(data_file_name,sep=',')
 names = df_data.columns
 print(len(names))
@@ -46,26 +47,35 @@ for i in range(num_dwba):
     y = df_dwba[i+1].to_numpy()
     tck.append(splrep(x, y, s=0))
 # %%
-numex = 2
-c2s = [5.,4.]
-fig = make_subplots(rows=2, cols=1, shared_xaxes=True, shared_yaxes=True, 
+numex = 5
+c2s = [5.,5.,1.5,5.,4.]
+dwbanum = [1,2,5,6,7]
+ymin=0.1
+ymax=3
+fig = make_subplots(rows=5, cols=1, shared_xaxes=True, shared_yaxes=True, 
                     horizontal_spacing=0.0,vertical_spacing=0.05)
 for i in range(numex):
-    fig.add_trace(go.Scatter(x = df_dwba[0],y=c2s[i]*df_dwba[6+i]),row=i+1,col=1)
+    fig.add_trace(go.Scatter(x = df_dwba[0],y=c2s[i]*df_dwba[dwbanum[i]]),row=i+1,col=1)
     fig.add_trace(go.Scatter(x = df_data[names[2*i]], y = df_data[names[2*i+1]],
             name=names[2*i+1],text=df_data[names[2*i+1]],error_y=dict(type='data', array=np.sqrt(df_data[names[2*i+1]])),
             mode='markers',marker_color=color[i],marker_size=10),
             row=i+1,col=1)
-fig.update_layout(width=700,height=600,showlegend=False,margin=dict(t=100,b=100,l=150,r=20),
+fig.update_layout(width=700,height=1000,showlegend=False,margin=dict(t=100,b=100,l=150,r=20),
                   font=dict(size=18, family='times'))
-fig.update_yaxes(title="<i>d&#963;/d&#x3A9;</i> [arb. units]",range=[0.8,2.5],mirror=True,ticks="inside",
+fig.update_yaxes(title="<i>d&#963;/d&#x3A9;</i> [arb. units]",range=[ymin,ymax],mirror=True,ticks="inside",
                  tickson="boundaries",ticklen=10,showline=True,zeroline=True,type="log",
                 ticktext=['10<sup>0 </sup> ','10<sup>1 </sup> ','10<sup>2 </sup> ','10<sup>3 </sup> ','10<sup>4 </sup> '],
                 tickvals=[1,10,100,1000,10000],showgrid=False)
-fig.update_xaxes(title="",range=[10,45],mirror=True,ticks="inside",
+fig.update_xaxes(title="",range=[10,50],mirror=True,ticks="inside",
                  tickson="boundaries",ticklen=10,showline=True,zeroline=True,showgrid=False,row=1,col=1)
-fig.update_xaxes(title="&#x1D703;<sub><i>cm</i></sub> [deg.]",range=[10,45],mirror=True,ticks="inside",
+fig.update_xaxes(title="",range=[10,50],mirror=True,ticks="inside",
                  tickson="boundaries",ticklen=10,showline=True,zeroline=True,showgrid=False,row=2,col=1)
+fig.update_xaxes(title="",range=[10,50],mirror=True,ticks="inside",
+                 tickson="boundaries",ticklen=10,showline=True,zeroline=True,showgrid=False,row=3,col=1)
+fig.update_xaxes(title="",range=[10,50],mirror=True,ticks="inside",
+                 tickson="boundaries",ticklen=10,showline=True,zeroline=True,showgrid=False,row=4,col=1)
+fig.update_xaxes(title="&#x1D703;<sub><i>cm</i></sub> [deg.]",range=[10,50],mirror=True,ticks="inside",
+                 tickson="boundaries",ticklen=10,showline=True,zeroline=True,showgrid=False,row=5,col=1)
 fig.show()
 # %%
 
