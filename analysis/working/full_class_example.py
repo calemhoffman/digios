baf570bf4c6690fe83e621d66ee0aee246595cf3
calemhoffman <calem.hoffman@gmail.com>
2024-03-06 22:42:31 -0600

    Update full_class_example.py

--- analysis/working/full_class_example.py
+++ analysis/working/full_class_example.py
@@ -21,6 +21,15 @@ from sklearn.preprocessing import StandardScaler
 from sklearn.svm import SVC
 from sklearn.tree import DecisionTreeClassifier
 
+import plotly.graph_objects as go
+from plotly.subplots import make_subplots
+import plotly.io as pio
+
+color = ['#E97371','#2C6A6A','#865740','#70808F','#B93E36','#F99183','#44938E','#B87843','#0E3A33']
+colorgrad = [color[0],color[1],color[2]]
+pio.templates["mycolor"] = go.layout.Template(layout_colorway=color)
+pio.templates.default = "mycolor"
+
 
 df = pd.read_csv('all_data.csv')
 df = df.dropna()
@@ -30,8 +39,8 @@ df_orig = df
 df = df[df['target']>0]
 df=df[df['target']<3]
 df = df[df['Ex']<8.0]
-X = X_orig[:200]
-y = y_orig[:200]
+X = X_orig[:100]
+y = y_orig[:100]
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 scaler = StandardScaler()
 X_train = scaler.fit_transform(X_train)
@@ -41,30 +50,30 @@ X_orig_scaled = scaler.transform(X_orig) #all data scaled
 #%%
 names = [
     "Nearest Neighbors",
-    # "Linear SVM",
-    # "RBF SVM",
-    # "Gaussian Process",
-    # "Decision Tree",
-    # "Random Forest",
-    # "Neural Net",
-    # "AdaBoost",
-    # "Naive Bayes",
-    # "QDA",
+    "Linear SVM",
+    "RBF SVM",
+    "Gaussian Process",
+    "Decision Tree",
+    "Random Forest",
+    "Neural Net",
+    "AdaBoost",
+    "Naive Bayes",
+    "QDA",
 ]
 
 classifiers = [
     KNeighborsClassifier(3),
-    # SVC(kernel="linear", C=0.025, random_state=42),
-    # SVC(gamma=2, C=1, random_state=42),
-    # GaussianProcessClassifier(1.0 * RBF(1.0), random_state=42),
-    # DecisionTreeClassifier(max_depth=5, random_state=42),
-    # RandomForestClassifier(
-    #     max_depth=5, n_estimators=10, max_features=1, random_state=42
-    # ),
-    # MLPClassifier(alpha=1, max_iter=1000, random_state=42),
-    # AdaBoostClassifier(algorithm="SAMME", random_state=42),
-    # GaussianNB(),
-    # QuadraticDiscriminantAnalysis(),
+    SVC(kernel="linear", C=0.025, random_state=42),
+    SVC(gamma=2, C=1, random_state=42),
+    GaussianProcessClassifier(1.0 * RBF(1.0), random_state=42),
+    DecisionTreeClassifier(max_depth=5, random_state=42),
+    RandomForestClassifier(
+        max_depth=5, n_estimators=10, max_features=1, random_state=42
+    ),
+    MLPClassifier(alpha=1, max_iter=1000, random_state=42),
+    AdaBoostClassifier(algorithm="SAMME", random_state=42),
+    GaussianNB(),
+    QuadraticDiscriminantAnalysis(),
 ]
 
 #%%
@@ -137,38 +146,33 @@ for name, clf in zip(names, classifiers):
         horizontalalignment="right",
     )
     i += 1
+    fig_pred = make_subplots(rows=1, cols=2)
+    for i in range(2):
+        fig_pred.add_trace(go.Scatter(x=[9.2,9.2], y=[0, 40],
+                            mode='lines', name='Mean', line=dict(color=color[6], width=3)),row=1,col=i+1)
+        fig_pred.add_trace(go.Scatter(x=[8.76,8.76], y=[0, 40],
+                            mode='lines', name='Mean', line=dict(color=color[7], width=3)),row=1,col=i+1)
+        fig_pred.add_trace(go.Histogram(x=df_orig[df_orig['target']==(i+1)].Ex,
+                                            xbins=dict(start=-1.0,
+                                            end=11.0,
+                                            size=0.1),marker=dict(color=color[0],opacity=0.5, line=dict(width=0))),
+                        row=1,col=i+1)
+        fig_pred.add_trace(go.Histogram(x=df_orig[df_orig['pred']==(i+1)].Ex,
+                                            xbins=dict(start=-1.0,
+                                            end=11.0,
+                                            size=0.1),marker=dict(color=color[1],opacity=0.5,line=dict(width=0))),
+                        row=1,col=i+1)
+        fig_pred.update_xaxes(range=[2,10],row=1,col=i+1)
+    fig_pred.update_layout(width=600,height=600,barmode='overlay')
+    fig_pred.show()
 ds_cnt+=1
 plt.tight_layout()
 plt.show()
 
+
+
 #%%
-import plotly.graph_objects as go
-from plotly.subplots import make_subplots
-import plotly.io as pio
 
-color = ['#E97371','#2C6A6A','#865740','#70808F','#B93E36','#F99183','#44938E','#B87843','#0E3A33']
-colorgrad = [color[0],color[1],color[2]]
-pio.templates["mycolor"] = go.layout.Template(layout_colorway=color)
-pio.templates.default = "mycolor"
 
-fig_pred = make_subplots(rows=1, cols=2)
-for i in range(2):
-    fig_pred.add_trace(go.Scatter(x=[9.2,9.2], y=[0, 40],
-                         mode='lines', name='Mean', line=dict(color=color[6], width=3)),row=1,col=i+1)
-    fig_pred.add_trace(go.Scatter(x=[8.76,8.76], y=[0, 40],
-                         mode='lines', name='Mean', line=dict(color=color[7], width=3)),row=1,col=i+1)
-    fig_pred.add_trace(go.Histogram(x=df_orig[df_orig['target']==(i+1)].Ex,
-                                        xbins=dict(start=-1.0,
-                                        end=11.0,
-                                        size=0.1),marker=dict(color=color[0],opacity=0.5, line=dict(width=0))),
-                       row=1,col=i+1)
-    fig_pred.add_trace(go.Histogram(x=df_orig[df_orig['pred']==(i+1)].Ex,
-                                        xbins=dict(start=-1.0,
-                                        end=11.0,
-                                        size=0.1),marker=dict(color=color[1],opacity=0.5,line=dict(width=0))),
-                       row=1,col=i+1)
-    fig_pred.update_xaxes(range=[2,10],row=1,col=i+1)
-fig_pred.update_layout(width=1000,height=800,barmode='overlay')
 
-fig_pred.show()
 # %%
