95e1c2618bc0ec35b90e9510e8d93bbe4662c4ca
Ryan@SOLARIS_testStation <rtang@fsu.edu>
2025-06-19 18:29:09 -0500

    almost done, still testing

--- analysis/EventBuilder/BinaryReader.h
+++ analysis/EventBuilder/BinaryReader.h
@@ -4,6 +4,7 @@
 #include <vector>
 #include <cstdint>
 #include <stdexcept>
+#include <algorithm>
 
 #include "Hit.h"  
 
@@ -16,7 +17,11 @@ public:
   }
 
   void Open(const std::string& filename);
+  std::string GetFileName() const { return fileName; }  // Get the file name
+  unsigned short GetDigID() const { return DigID; }  // Get the last three digits of the file name
+  unsigned short GetFileIndex() const { return fileIndex; }  // Get the second last three digits of the file name
   size_t GetFileSize() const { return fileSize; }  // Get size of the file in bytes
+
   template <typename T> T Read();
   template <typename T> std::vector<T> ReadArray(std::size_t count);
   std::vector<char> ReadBytes(std::size_t count);
@@ -26,8 +31,8 @@ public:
   size_t Tell() { return static_cast<size_t>(file.tellg()); }  // Get current file position
   void Seek(std::streampos pos) { file.seekg(pos); }   // Seek to position
   
-  void Scan(bool quick = false);  // Scan the file to count data blocks
-  unsigned int GetNumData() const { return numData; }  // Get number of data blocks found
+  void Scan(bool quick = false, bool debug = false);  // Scan the file to count data blocks
+  unsigned int GetNumData() const { return totalNumHits; }  // Get number of data blocks found
   const std::vector<Hit>& GetHits() const { return hits; }  // Get vector of hits
   Hit GetHit(unsigned int index) const {
     if (index < hits.size()) {
@@ -37,48 +42,59 @@ public:
     }
   }
 
+  void ClearHits() { hits.clear(); }  // Clear the hits vector
   size_t GetMemoryUsageBytes();
+
+  void ReadNextNHitsFromFile(unsigned int n);
 
 private:
   std::ifstream file;
+  std::string fileName;
   size_t fileSize; // Size of the file in bytes
+  unsigned short DigID; // the last three digits of the file name, e.g. 001, 002, etc.
+  unsigned short fileIndex; // the 2nd last three digits of the file name, e.g. 000, 001, etc. to indicate the next file from the same DigID.
 
-  unsigned int numData;
+  unsigned int totalNumHits;
+  unsigned int hitID; // current hit ID
 
   std::vector<Hit> hits; // Vector to store hits
 
   void Init(){
     fileSize = 0;
-    numData = 0;
+    DigID = 0;
+    fileIndex = 0;
+    totalNumHits = 0;
+    hitID = 0;
+    hits.clear();
   }
 };
 
 //########################################################################
 
-inline void BinaryReader::Scan(bool quick) {
+inline void BinaryReader::Scan(bool quick, bool debug) {
 
   Hit hit;
-  numData = 0;
+  totalNumHits = 0;
   unsigned filePos = 0; // byte
   uint32_t type = 0;
   uint64_t old_timestamp = 0;
   unsigned int timestamp_error_counter = 0;
   do{
     hit.header = Read<GEBHeader>();
-    if( numData == 0) type = hit.header.type;
+    if( totalNumHits == 0) type = hit.header.type;
     
-    // printf("Data ID : %d \n", numData);
+    // printf("Data ID : %d \n", totalNumHits);
     // header.Print();
     
     if( hit.header.type != type) {
-      printf("header error at Data ID : %d \n", numData);
+      printf("header error at Data ID : %d \n", totalNumHits);
       break;
     }
 
     if( hit.header.timestamp < old_timestamp) {
-      printf("timestamp error at Data ID : %d \n", numData);
-      printf("old timestamp : %16lu \n", old_timestamp);
-      printf("new timestamp : %16lu \n", hit.header.timestamp);
+      if( debug ) printf("timestamp error at Data ID : %d \n", totalNumHits);
+      if( debug ) printf("old timestamp : %16lu \n", old_timestamp);
+      if( debug ) printf("new timestamp : %16lu \n", hit.header.timestamp);
       timestamp_error_counter ++;
     }
     old_timestamp = hit.header.timestamp;
@@ -90,16 +106,54 @@ inline void BinaryReader::Scan(bool quick) {
       hits.push_back(hit); // Store the hit in the vector
     }
 
-    numData ++;
+    totalNumHits ++;
   }while(Tell() < fileSize);
 
-  printf("number of Data Found : %d \n", numData);
-  if( timestamp_error_counter > 0 ) printf("timestamp error found for %u times.\n", timestamp_error_counter);
-  printf("go back to beginging of the file\n");
+  if( debug ) printf("number of Hit Found : %d \n", totalNumHits);
+  if( timestamp_error_counter > 0 && debug ) printf("timestamp error found for %u times.\n", timestamp_error_counter);
   file.seekg(0, std::ios::beg);
+  hitID = 0; // Reset hitID to 0 after scanning
 
 }
 
+inline void BinaryReader::ReadNextNHitsFromFile(unsigned int n) {
+
+  printf("Reading next %u hits from file: %s |", n, fileName.c_str());
+  hits.clear();  // Clear previous hits
+  uint64_t old_timestamp = 0;
+  unsigned int timestamp_error_counter = 0;
+  for (unsigned int i = 0; i < n && Tell() < fileSize; ++i) {
+    Hit hit;
+    hit.header = Read<GEBHeader>();
+
+    if( hit.header.timestamp < old_timestamp) {
+      printf("timestamp error at Data ID : %d \n", i);
+      printf("old timestamp : %16lu \n", old_timestamp);
+      printf("new timestamp : %16lu \n", hit.header.timestamp);
+      timestamp_error_counter++;
+    }
+    old_timestamp = hit.header.timestamp;
+
+    if (hit.header.payload_lenght_byte > 0) {
+      hit.payload = ReadArray<uint32_t>(hit.header.payload_lenght_byte / sizeof(uint32_t));
+      hits.push_back(hit);  // Store the hit in the vector
+    }
+  }
+
+  //sort hits by timestamp
+  if( timestamp_error_counter > 0 ) {
+    printf("timestamp error found for %u times. Sort data.\n", timestamp_error_counter);
+    std::sort(hits.begin(), hits.end(), [](const Hit& a, const Hit& b) {
+      return a.header.timestamp < b.header.timestamp;
+    });
+  }
+  hitID += n;  // Update the hitID to the next position
+
+  printf(" first timestamp: %lu, last timestamp: %lu, total hits read: %zu\n", 
+         hits.front().header.timestamp, 
+         hits.back().header.timestamp, 
+         hits.size());
+}
 
 inline size_t BinaryReader::GetMemoryUsageBytes() {
   size_t total = sizeof(BinaryReader);
@@ -117,12 +171,33 @@ inline void BinaryReader::Open(const std::string& filename){
   if (!file) {
     throw std::runtime_error("Failed to open file: " + filename);
   }
+  // printf("============== Opened file: %s\n", filename.c_str());
+  this->fileName = filename;
   file.seekg(0, std::ios::end);
   fileSize = file.tellg();
   file.seekg(0, std::ios::beg);
   if (fileSize < sizeof(GEBHeader)) {
     throw std::runtime_error("File is too small to contain a GEBHeader.");
   }
+  //decode the DigID and fileIndex from the filename
+  std::string baseName = filename.substr(filename.find_last_of("/\\") + 1);
+  if (baseName.length() >= 6) {
+    try {
+      DigID = std::stoi(baseName.substr(baseName.length() - 3, 3));
+      fileIndex = std::stoi(baseName.substr(baseName.length() - 7, 3));
+    } catch (const std::invalid_argument & e) {
+      throw std::runtime_error("Failed to parse DigID and fileIndex from filename: " + filename);
+    }
+  } else {
+    throw std::runtime_error("Filename is too short to contain DigID and fileIndex: " + filename);
+  }
+  // printf("Opened file: %s\n", filename.c_str());
+  // printf("DigID: %03d, fileIndex: %03d\n", DigID, fileIndex);
+  // if (fileSize > 0) {
+  //   printf("File size: %zu bytes\n", fileSize);
+  // } else {
+  //   throw std::runtime_error("File is empty: " + filename); 
+  // }
 }
 
 template <typename T> inline T BinaryReader::Read() {
--- analysis/EventBuilder/EventBuilder.cpp
+++ analysis/EventBuilder/EventBuilder.cpp
@@ -16,6 +16,19 @@ inline unsigned int getTime_us(){
   return time_us;
 }
 
+
+#define MAX_MULTI 64
+#define MAX_TRACE_LEN 1250 ///  = 10 us
+
+//unsigned long long                        evID = 0;
+unsigned int                            numHit = 0;
+unsigned short                   id[MAX_MULTI] = {0}; 
+unsigned short      pre_rise_energy[MAX_MULTI] = {0};  
+unsigned short     post_rise_energy[MAX_MULTI] = {0};  
+unsigned long long        timestamp[MAX_MULTI] = {0};
+unsigned int               traceLen[MAX_MULTI] = {0};
+unsigned short trace[MAX_MULTI][MAX_TRACE_LEN] = {0};
+
 //^============================================
 int main(int argc, char* argv[]) {
 
@@ -45,15 +58,186 @@ int main(int argc, char* argv[]) {
   }
 
   //*=============== setup reader
+  uint64_t totalNumHits = 0;
   BinaryReader * reader = new BinaryReader[nFile];
   for( int i = 0 ; i < nFile ; i++){
     reader[i].Open(inFileName[i].Data());
-    reader[i].Scan();
-    const auto& hits = reader[i].GetHits();
-    printf("File %s has %u hits\n", inFileName[i].Data(), reader[i].GetNumData());
+    reader[i].Scan(true);
+    totalNumHits += reader[i].GetNumData();
+    // printf("File %s has %u hits\n", inFileName[i].Data(), reader[i].GetNumData());
+  }
+  
+  //*=============== group files by DigID and sort the fileIndex
+  std::map<unsigned short, std::vector<BinaryReader*>> fileGroups;
+  for( int i = 0 ; i < nFile ; i++){
+    unsigned short digID = reader[i].GetDigID();
+    if (!fileGroups.count(digID) ) {
+      fileGroups[digID] = std::vector<BinaryReader*>();
+    }
+    fileGroups[digID].push_back(&reader[i]);
+  }
+  printf("Found %zu DigIDs\n", fileGroups.size());
+
+  // printf out the file groups
+  for( const std::pair<const unsigned short, std::vector<BinaryReader*>>& group : fileGroups) { // looping through the map
+    unsigned short digID = group.first; // DigID
+    const auto& readers = group.second; // Vector of BinaryReader pointers
+    printf("----- DigID %03d has %zu files\n", digID, readers.size());
+    for (size_t j = 0; j < readers.size(); j++) {
+      printf("  File %zu: %s | num. of hit : %d\n", j, readers[j]->GetFileName().c_str(), readers[j]->GetNumData());
+    }
   }
 
+  printf("================= Total number of hits: %lu\n", totalNumHits);
+
+  //*=============== create output file and setup TTree
+  // TFile * outFile = TFile::Open(outFileName.Data(), "RECREATE");
+  
+  // TTree * outTree = new TTree("tree", outFileName.Data());
+  // outTree->SetAutoSave(10000000); // autosave every 10 million bytes
+  // outTree->SetAutoFlush(1000000); // auto-flush every 1 million bytes 
+  // outTree->SetDirectory(outFile);
+
+  // outTree->Branch("numHit", &numHit, "numHit/i");
+  // outTree->Branch("id", id, "id[numHit]/s");
+  // outTree->Branch("pre_rise_energy", pre_rise_energy, "pre_rise_energy[numHit]/s");
+  // outTree->Branch("post_rise_energy", post_rise_energy, "post_rise_energy[numHit]/s");
+  // outTree->Branch("timestamp", timestamp, "timestamp[numHit]/l");
+  // if( saveTrace ){
+  //   outTree->Branch("traceLen", traceLen, "traceLen[numHit]/i");
+  //   outTree->Branch("   trace",    trace, "trace[numHit][traceLen[numHit]]/s");
+  // } 
 
 
+  //*=============== read n data from each file
+  const unsigned int MAX_READ_HITS = 10000; // Maximum hits to read at a time
+  
+  
+  uint64_t earlistTime = UINT64_MAX; 
+  int earlistDigID = -1; // Earliest DigID with the earliest timestamp
+  std::map<unsigned short, unsigned int> hitID; // store the hit ID for the current reader for each DigID
+  std::map<unsigned short, short> fileID; // store the file ID for each DigID
+  
+  for( const std::pair<const unsigned short, std::vector<BinaryReader*>>& group : fileGroups) { // looping through the map
+    unsigned short digID = group.first; // DigID
+    const auto& readers = group.second; // Vector of BinaryReader pointers
+
+    hitID[digID] = 0; // Initialize hitID for this DigID
+    fileID[digID] = 0; 
+
+    BinaryReader* reader = readers[0];
+    reader->ReadNextNHitsFromFile(10000); // Read 10,000 hits at a time
+
+    if( reader->GetHit(0).header.timestamp < earlistTime ) {
+      earlistTime = reader->GetHit(0).header.timestamp; // Update the earliest timestamp
+      earlistDigID = digID; // Update the earliest DigID
+    }
+
+    totalNumHits += reader->GetNumData();
+  }
+
+  
+  //*=============== event building
+  std::vector<Event> events; // Vector to store events
+  unsigned int eventID = 0;
+  
+  int debug = 0;
+  int debug2 = 10; 
+  do{
+
+    printf("##### Earliest timestamp: %lu from DigID %03d\n", earlistTime, earlistDigID);
+
+    for( const std::pair<const unsigned short, std::vector<BinaryReader*>>& group : fileGroups) { // looping through the map
+      unsigned short digID = group.first; // DigID
+      if( fileID[digID] < 0 ) continue; // Skip if no more files for this DigID
+      const std::vector<BinaryReader*>& readers = group.second; // Vector of BinaryReader pointers
+
+      const std::vector<Hit> hits = readers[fileID[digID]]->GetHits();
+      
+      if( hitID[digID] >= hits.size() ) {
+        readers[0]->ReadNextNHitsFromFile(MAX_READ_HITS); // Read more hits if needed
+        hitID[digID] = 0; // Reset hitID for this DigID
+      }
+
+      if( hits.empty() ) { // load next file if no hits
+        if( fileID[digID] < readers.size() - 1 ) {
+          fileID[digID]++;
+          readers[fileID[digID]]->ReadNextNHitsFromFile(MAX_READ_HITS); // Read more hits from the next file
+          hitID[digID] = 0; // Reset hitID for this DigID
+        } else {
+          printf("No more files for DigID %03d\n", digID);
+          fileID[digID] = -1; // Mark that there are no more files for this DigID
+          continue; // No more files to read for this DigID
+        }
+      }
+
+      
+      if( timeWindow < 0 ) {
+        printf("Processing DigID %03d, Hit ID %u, file ID %d, Timestamp %lu\n", digID, hitID[digID], fileID[digID], hits[hitID[digID]].header.timestamp);
+        events.push_back(hits[hitID[digID]].DecodePayload()); // Decode the hit payload and store it in the events vector
+        numHit++;
+        hitID[digID]++; // Move to the next hit
+        break;
+      }
+      
+      for (int i = hitID[digID]; i < hits.size(); i++) {  
+        printf("Processing DigID %03d, Hit ID %u, file ID %d, Timestamp %lu\n", digID, hitID[digID], fileID[digID], hits[hitID[digID]].header.timestamp);
+        if( hits[i].header.timestamp - earlistTime  <= timeWindow) {
+          events.push_back(hits[i].DecodePayload()); // Decode the hit payload and store it in the events vector
+          numHit++;
+          hitID[digID]++; // Move to the next hit
+        }else{
+          printf("----- break\n");
+          break;
+        }      
+      }
+    }
+
+    printf("EventID : %u, Number of Hits in this event : %zu| Total hits read: %u\n", eventID, events.size(), numHit);
+
+    //TODO save to TTree
+    if( events.size() > 0 ) {
+      std::sort(events.begin(), events.end(), [](const Event& a, const Event& b) {
+        return a.timestamp < b.timestamp; // Sort events by timestamp
+      });
+    }
+
+
+    // prepare for the next event
+    eventID ++;
+    events.clear(); // Clear the events vector for the next event
+
+    // find the next earliest timestamp
+    earlistTime = UINT64_MAX;
+    earlistDigID = -1; // Reset the earliest DigID
+    for( const std::pair<const unsigned short, std::vector<BinaryReader*>>& group : fileGroups) { // looping through the map
+      unsigned short digID = group.first; // DigID
+      const auto& readers = group.second; // Vector of BinaryReader pointers    
+
+      if( fileID[digID] < 0 ) continue; // Skip if no more files for this DigID
+
+      BinaryReader* reader = readers[fileID[digID]];
+      if( hitID[digID] < reader->GetNumData() ) {
+        if( reader->GetHit(hitID[digID]).header.timestamp < earlistTime ) {
+          earlistTime = reader->GetHit(hitID[digID]).header.timestamp; // Update the earliest timestamp
+          earlistDigID = digID; // Update the earliest DigID
+        }
+      } else {
+        if( fileID[digID] < readers.size() - 1 ) {
+          fileID[digID]++;
+          reader = readers[fileID[digID]];
+          if( reader->GetHit(0).header.timestamp < earlistTime ) {
+            earlistTime = reader->GetHit(0).header.timestamp; // Update the earliest timestamp
+            earlistDigID = digID; // Update the earliest DigID
+          }
+        }else{
+          fileID[digID] = -1; // Mark that there are no more files for this DigID
+        }
+      }
+    }
+
+  }while(debug < debug2 && earlistDigID >= 0);
+  
+  
   return 0;
 }
