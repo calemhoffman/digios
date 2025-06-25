672efb74b22fbbc75ea21085c7a0ecfbd23a46c2
Ryan@SOLARIS_testStation <rtang@fsu.edu>
2025-06-25 13:41:45 -0500

    checked trace saving and modify readRawTrace

--- analysis/EventBuilder/script.sh
+++ analysis/EventBuilder/script.sh
@@ -5,13 +5,16 @@
 # ./EventBuilder test.root 1000 0 `\ls -1 run_107*`
 # ./EventBuilder test.root 1000 0 run_107.gtd04_000_0113 run_107.gtd04_001_0113
 
-# exit
+# ./EventBuilder test_trace.root 1000 1 `\ls -1 ../data/*run_040*`
+./EventBuilder test_trace.root 100000000 1 ../data/h093_21F_d3He_run_040.gtd02_000_0105
+
+exit
 
 #======== GEB Merge and Sorte
 start=$(date +%s%N)
-../GEBSort/GEBMerge ../working/GEBMerge.chat  merged_file `\ls -1 run_107*`
+../GEBSort/GEBMerge ../working/GEBMerge.chat  merged_file `\ls -1 ../data/*run_040*`
 
-../GEBSort/EventBuilder -input disk merged_file_000 -rootfile  sorted.root -chat ../working/GEBSort.chat
+../GEBSort/EventBuilder_trace -input disk merged_file_000 -rootfile  trace.root -chat ../working/GEBSort.chat
 
 end=$(date +%s%N)
 elapsed=$(( (end - start) / 1000 ))  # convert nanoseconds to microseconds
