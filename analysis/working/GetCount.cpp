665a3ebf9738b9237d1a17516767181d72e002e0
Ryan@Home <goluckyryan@gmail.com>
2026-01-25 12:59:40 -0600

    after analysis of Run9, ground state and 4 MeV states

--- analysis/working/GetCount.cpp
+++ analysis/working/GetCount.cpp
@@ -1,10 +1,12 @@
+#include <cstdio>
+#include "Monitors.C"
 
-{
+void GetCOunt(){
 
   double w = 0.05;
   double xRangeMin = -2;
   
-  double xCenter = 4.44;
+  double xCenter = 0;
 
   double xMin = xCenter -0.2;
   double xMax = xCenter + 0.2;
@@ -12,10 +14,15 @@
   int binMin = (xMin - xRangeMin) / w + 1;
   int binMax = (xMax - xRangeMin) / w + 1;
 
+  double totalCount = 0;
+
   for (int i = 0; i < 24; i++){
     double value = hExi[i]->Integral(binMin, binMax);
     printf("DetID %2d: Count = %.0f\n", i, value);
+    totalCount += value;
     if( i % 6 == 5 ) printf("\n");
   }
+
+  printf("Total Count = %.0f\n", totalCount);
 
 }
