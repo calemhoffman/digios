f250b1e2808e98f0c53eedbf379d77acd64af45d
General.Helios@pi5-2 <Dudu@gmail.compi5-2>
2026-04-10 17:56:59 -0500

    daq: push2Discord.sh convert HTML to Discord Markdown, fix empty RunNum

--- daq/push2Discord.sh
+++ daq/push2Discord.sh
@@ -7,16 +7,31 @@ source ~/Discord_webhooks.sh
 source ~/elogID.txt
 elogID=${ID}
 
+# Get run number from expName.sh
+source ~/digios/expName.sh 2>/dev/null
+runNum=$((LastRunNum))
+
+# Convert HTML to Discord Markdown
+html_to_markdown() {
+    echo "$1" | \
+        sed 's/<b>/\*\*/g' | \
+        sed 's/<\/b>/\*\*/g' | \
+        sed 's/<br\/>/\n/g' | \
+        sed 's/<br \/>/\n/g' | \
+        sed 's/<[^>]*>//g' | \
+        sed 's/"/\\"/g'
+}
+
 if [ $isStartRun -eq 1 ]; then
-    slackMsg="**RUN=${LastRunNum}**.  https://www.phy.anl.gov/elog/${elogName}/${elogID}\n"
-    elogMsg=`cat ~/elog.txt`
-    elogMsg=$(echo "$elogMsg" | sed 's/\*/=/g' | sed -e 's/<br \/>//g' | sed 's/"/\\"/g; s/$/\\n/' | tr -d '\n' | sed 's/\\n$//')
+    slackMsg="**RUN=${runNum}**  https://www.phy.anl.gov/elog/${elogName}/${elogID}\n"
+    elogMsg=$(cat ~/elog.txt)
+    elogMsg=$(html_to_markdown "$elogMsg" | sed 's/$/\\n/' | tr -d '\n' | sed 's/\\n$//')
     curl -H "Content-Type: application/json" -X POST -d "{\"content\":\"${slackMsg}${elogMsg}\"}" $WEBHOOK_DAQ_URL
 
 else
 
     slackMsg="https://www.phy.anl.gov/elog/${elogName}/${elogID}\n"
-    auxMsg=`cat ~/.endRunMsg.txt`
+    auxMsg=$(cat ~/.endRunMsg.txt 2>/dev/null)
     curl -H "Content-Type: application/json" -X POST -d "{\"content\":\"${slackMsg}${auxMsg}\"}" $WEBHOOK_DAQ_URL
 
 fi
